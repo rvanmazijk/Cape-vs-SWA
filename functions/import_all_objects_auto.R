4f7fcbc23b3916a2642edc64e1d5d430ee31d682
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-06-21 15:31:38 +0200

    Refactor import_all_objects_auto.R

--- functions/import_all_objects_auto.R
+++ functions/import_all_objects_auto.R
@@ -2,23 +2,15 @@ import_all_objects_auto <- function(path) {
     files <- list.files(path, full.names = TRUE)
     if (length(files) == 0) {
         message("Empty folder")
-    }
-    for (file in files) {
-        if (str_detect(file, "\\.csv")) {
-            assign(
-                # Trim path to only filename, w/o file-extention
-                x = str_remove(str_extract(file, "[^\\/]+\\.csv"), "\\.csv"),
-                value = read_csv(file),
-                envir = .GlobalEnv
+    } else {
+        for (file in files) {
+            object_name <- xfun::sans_ext(basename(file))
+            ext <- tolower(xfun::file_ext(file))
+            object <- switch(ext,
+                "csv" = readr::read_csv(file),
+                "rds" = readr::read_rds(file)
             )
-        } else if (str_detect(file, "\\.RDS")) {
-            assign(
-                x = str_remove(str_extract(file, "[^\\/]+\\.RDS"), "\\.RDS"),
-                value = read_rds(file),
-                envir = .GlobalEnv
-            )
-        } else {
-            print(glue("{file} neither a CSV nor RDS!"))
+            assign(object_name, object, envir = .GlobalEnv)
         }
     }
 }
