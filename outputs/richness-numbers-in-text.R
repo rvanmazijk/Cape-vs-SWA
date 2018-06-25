29107cda508dc79c68ebdac868fa2d11f6f1d762
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-06-25 15:31:04 +0200

    Make figures and in-text richness numbers object

--- outputs/richness-numbers-in-text.R
+++ outputs/richness-numbers-in-text.R
@@ -1,54 +1,42 @@
-GCFR_spp_sets <- readRDS(here::here(
-    "Data",
-    "derived-data",
-    "flora",
-    "GCFR_spp_sets_local.rds"
-))
-SWAFR_spp_sets <- readRDS(here::here(
-    "Data",
-    "derived-data",
-    "flora",
-    "SWAFR_spp_sets_local.rds"
-))
-GCFR_names_CRAP <- read.csv(here::here(
-    "Data",
-    "derived-data",
-    "flora",
-    "GCFR_names_CRAP.csv"
-))
-SWAFR_names_CRAP <- read.csv(here::here(
-    "Data",
-    "derived-data",
-    "flora",
-    "SWAFR_names_CRAP.csv"
-))
-GCFR_clean_flora <- read.csv(here::here(
-    "Data",
-    "derived-data",
-    "flora",
-    "GCFR_clean_flora_2017-09-14.csv"
-))
-SWAFR_clean_flora <- read.csv(here::here(
-    "Data",
-    "derived-data",
-    "flora",
-    "SWAFR_clean_flora_2017-09-14.csv"
-))
+# Import species lists
+# FIXME
+# GCFR_spp_sets <- readRDS(here::here("data/derived-data/flora/GCFR_spp_sets_local.rds"))
+# SWAFR_spp_sets <- readRDS(here::here("data/derived-data/flora/SWAFR_spp_sets_local.rds"))
+# GCFR_names_CRAP <- read.csv(here::here("data/derived-data/flora/GCFR_names_CRAP.csv"))
+# SWAFR_names_CRAP <- read.csv(here::here("data/derived-data/flora/SWAFR_names_CRAP.csv"))
+GCFR_clean_flora <- read_csv(here::here("data/derived-data/flora/GCFR_clean_flora_2017-09-14.csv"))
+SWAFR_clean_flora <- read_csv(here::here("data/derived-data/flora/SWAFR_clean_flora_2017-09-14.csv"))
+
 # Summarise for use in-text
-GBIF_in_text <- list(
-    pre_accepted_N = list(
-        GCFR = GCFR_spp_sets$species,
-        SWAFR = SWAFR_spp_sets$species
+richness <- list(
+    # FIXME
+    n_before_checks = list(
+        GCFR = "TODO", #GCFR_spp_sets$species,
+        SWAFR = "TODO" #SWAFR_spp_sets$species
     ),
-    CRAP_names = list(
-        GCFR = GCFR_names_CRAP$submitted_name,
-        SWAFR = SWAFR_names_CRAP$submitted_name
+    n_bad_names = list(
+        GCFR = "TODO", #GCFR_names_CRAP$submitted_name,
+        SWAFR = "TODO" #SWAFR_names_CRAP$submitted_name
     ),
-    final_N = list(
+    n_final = list(
         GCFR = GCFR_clean_flora$species,
         SWAFR = SWAFR_clean_flora$species
     )
 )
-GBIF_in_text %<>%
+richness %<>%
     map(map, unique) %>%
     map(map, length)
+
+# Save
+write_rds(richness, here::here("outputs/richness.RDS"))
+
+# Tidy up
+rm(
+    # FIXME
+    #GCFR_spp_sets,
+    #SWAFR_spp_sets,
+    #GCFR_names_CRAP,
+    #SWAFR_names_CRAP,
+    GCFR_clean_flora,
+    SWAFR_clean_flora
+)
