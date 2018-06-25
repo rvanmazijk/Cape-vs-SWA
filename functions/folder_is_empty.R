e0ee810749533b548f647aae865875d2988f6cfa
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-06-25 14:25:10 +0200

    Functionalise model tables, move some tables to appendix, and more!

--- functions/folder_is_empty.R
+++ functions/folder_is_empty.R
@@ -1,10 +1,13 @@
-folder_is_empty <- function(...) {
+folder_is_empty <- function(..., ignore = ".R") {
     paths <- c(...)
     out <- vector(length = length(paths))
     for (i in seq_along(paths)) {
-        out[[i]] <-
-            !file.exists(paths[[i]]) &
-            length(list.files(paths[[i]])) == 0
+        path_exists <- file.exists(paths[[i]])
+        files_in_path <- list.files(
+            paths[[i]],
+            pattern = glue::glue("[^\\{ignore}]$")
+        )
+        out[[i]] <- (!path_exists) & (length(files_in_path) == 0)
     }
     return(out)
 }
