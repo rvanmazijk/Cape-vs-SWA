0594abd3aa893498e4f9f624ea64e439105f7973
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-07-03 17:02:55 +0200

    Begin making maps

---
+++ figures/make-maps.R
@@ -0,0 +1,55 @@
+# Make maps of environmental and floral data
+# Cape vs SWA publication
+# Ruan van Mazijk
+
+# Setup ------------------------------------------------------------------------
+
+map(
+  c(here::here("figures/figure-setup.R"), pre_analysis_import_paths),
+  source
+)
+
+# Cape -------------------------------------------------------------------------
+
+GCFR_environment_plots <- vector("list", length = length(var_names))
+for (i in seq_along(var_names)) {
+  absolute_panel <- gplot(GCFR_variables_QDS[[i]]) +
+    geom_tile(aes(fill = value)) +
+    ggspatial::geom_spatial(GCFR_border, fill = NA, col = "black") +
+    scale_fill_distiller(palette = "Spectral", na.value = "white") +
+    labs(x = "Longitude (º)",
+         y = "Latitude (º)",
+         title = var_names[[i]]) +
+    theme(legend.position = c(0.99, 0.99),
+          legend.justification = c(1, 1),
+          legend.title = element_blank())
+  roughness_panel <- gplot(focal_sd(GCFR_variables_QDS[[i]])) +
+    geom_tile(aes(fill = value)) +
+    ggspatial::geom_spatial(GCFR_border, fill = NA, col = "black") +
+    scale_fill_distiller(palette = "Spectral", na.value = "white") +
+    labs(x = "Longitude (º)",
+         y = "Latitude (º)",
+         title = glue("{var_names[[i]]} roughness")) +
+    theme(legend.position = c(0.99, 0.99),
+          legend.justification = c(1, 1),
+          legend.title = element_blank())
+  GCFR_environment_plots[[i]] <- plot_grid(absolute_panel, roughness_panel)
+}
+ggsave(here::here("figures/Cape-enviro-maps.png"),
+  plot_grid(ncol = 2, plotlist = list(
+    plot_grid(ncol = 1, plotlist =
+      GCFR_environment_plots[1:5]
+    ),
+    plot_grid(ncol = 1, rel_heights = c(4, 1), plotlist = list(
+      plot_grid(ncol = 1, plotlist =
+        GCFR_environment_plots[6:9]
+      ),
+      grid.rect(gp = gpar(col = "white"))
+    ))
+  )),
+  width = 10, height = 12,
+  dpi = 300
+)
+
+ggosm() +
+  geom_spatial(focal_sd(GCFR_variables$Elevation))
