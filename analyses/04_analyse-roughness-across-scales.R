e531d299845d30f26a47038d8848aa2ef8579ff4
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-07-04 13:58:35 +0200

    Generate new roughness analysis outputs

--- analyses/04_analyse-roughness-across-scales.R
+++ analyses/04_analyse-roughness-across-scales.R
@@ -73,7 +73,7 @@ data_for_violin_plot <- foreach(resolution = list(0.05, 0.25, 0.50, 0.75)) %do% 
     cbind(region = "SWAFR", map2_df(SWAFR_variables, resolution, prep_layer))
   )
 }
-data_for_violin_plot <- data_for_violin_plot %$%
+data_for_violin_plot_tidy <- data_for_violin_plot %$%
   rbind(cbind(resolution = "0.05º", .[[1]]),
         cbind(resolution = "QDS",   .[[2]]),
         cbind(resolution = "HDS",   .[[3]]),
@@ -82,21 +82,20 @@ data_for_violin_plot <- data_for_violin_plot %$%
   gather(variable, roughness, -resolution, -region) %>%
   na.omit() %>%
   group_by(resolution, variable) %>%
-  mutate(z_roughness = scale(roughness))  # Z-scale that shit!
-data_for_violin_plot$variable %<>% factor(levels = var_names)
-data_for_violin_plot %<>% mutate(region = ifelse(region == "GCFR", "Cape", "SWA"))
+  mutate(z_roughness = scale(roughness)) %>%  # Z-scale!
+  ungroup() %>%
+  mutate(variable = factor(variable, levels = var_names),
+         region = ifelse(region == "GCFR", "Cape", "SWA"))
 
 # Save to disc
 write_csv(
-  data_for_violin_plot,
+  data_for_violin_plot_tidy,
   here::here("outputs/04_roughness-across-scales/data_for_violin_plot.csv")
 )
 
 # Analyse IQ95R (and IQ99R) ~ scale --------------------------------------------
 
-IQ95R_data <- data_for_violin_plot %>%
-  group_by(resolution, variable) %>%
-  mutate(z_roughness = scale(roughness)) %>%  # Z-scale that shit!
+IQ95R_data <- data_for_violin_plot_tidy %>%
   group_by(resolution, region, variable) %>%
   summarise(IQ99R = IQ99R(z_roughness),
             IQ95R = IQ95R(z_roughness)) %>%
@@ -106,12 +105,12 @@ IQ95R_data <- data_for_violin_plot %>%
                            0.99,
                            ifelse(quantile == "IQ95R",
                                   0.95,
-                                  NA)))
-IQ95R_data %<>% mutate(region = ifelse(region == "GCFR", "Cape", "SWA"))
+                                  NA))) %>%
+  ungroup() %>%
+  mutate(region = ifelse(region == "GCFR", "Cape", "SWA"))
 
 # Save to disc
 write_csv(
   IQ95R_data,
   here::here("outputs/04_roughness-across-scales/IQ95R_data.csv")
 )
-#    geom_smooth(method = "lm")
