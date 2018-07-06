3216dc0698fdb8c291a0cbce06eb0eb7f3d65dfd
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-07-06 14:06:03 +0200

    Add import calls for buffered region shapefiles

--- analyses/01_import-region-polygons.R
+++ analyses/01_import-region-polygons.R
@@ -6,6 +6,7 @@
 
 GCFR_border <- readOGR(here::here("data/derived-data/borders/GCFR_border/"))
 stopifnot(proj4string(GCFR_border) == std_CRS)
+GCFR_border_buffered <- readOGR(here::here("data/derived-data/borders/GCFR_border_buffered/"))
 GCFR_box <- readOGR(here::here("data/derived-data/borders/GCFR_box/"))
 GCFR_QDS <- readOGR(here::here("data/derived-data/borders/GCFR_QDS/"))
 
@@ -13,5 +14,6 @@ GCFR_QDS <- readOGR(here::here("data/derived-data/borders/GCFR_QDS/"))
 
 SWAFR_border <- readOGR(here::here("data/derived-data/borders/SWBP_Mike-Cramer/"))
 stopifnot(proj4string(SWAFR_border) == std_CRS)
+SWAFR_border_buffered <- readOGR(here::here("data/derived-data/borders/SWAFR_border_buffered/"))
 SWAFR_box <- readOGR(here::here("data/derived-data/borders/SWAFR_box/"))
 SWAFR_QDS <- readOGR(here::here("data/derived-data/borders/SWAFR_QDS/"))
