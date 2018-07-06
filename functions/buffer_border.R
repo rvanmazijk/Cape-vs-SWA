b72ab2bf845463234301c5e2b9661c63ca20e84c
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-07-06 15:58:39 +0200

    Update buffer_border() [in progress]

--- functions/buffer_border.R
+++ functions/buffer_border.R
@@ -1,7 +1,22 @@
+simple_SpatialPolygonsDataFrame <- function(x, value = "val",
+                                            row_names = names(x)) {
+  stopifnot(class(x) == "SpatialPolygons")
+  x_data <- data.frame(x = value, row.names = row_names)
+  SpatialPolygonsDataFrame(x, x_data)
+}
+
 buffer_border <- function(border, region = c("GCFR", "SWAFR"),
                           width = 0.1, dissolve = TRUE) {
   buffered_border <- raster::buffer(border, width = width, dissolve = dissolve)
-  border_data <- data.frame(region = region, row.names = names(buffered_border))
-  buffered_border %<>% SpatialPolygonsDataFrame(border_data)
-  buffered_border
+  simple_SpatialPolygonsDataFrame(buffered_border, region)
+}
+
+tidy_border <- function(border) {
+  remove_holes <- function(x) {
+    holes <- unlist(map(x, ~ .@hole))
+    x[!holes]
+  }
+  border %<>% raster::buffer(width = 0, dissolve = TRUE)
+  border@polygons[[1]]@Polygons %<>% remove_holes()
+  simple_SpatialPolygonsDataFrame(border)
 }
