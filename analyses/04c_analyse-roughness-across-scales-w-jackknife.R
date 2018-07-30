a590dd44f2303dd79347bc76a4b0295a3fcf0adb
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-07-30 13:29:35 +0200

    Implement full pipeline of jackknifed CLES (except for 0.05º scale... WIP)

--- analyses/04c_analyse-roughness-across-scales-w-jackknife.R
+++ analyses/04c_analyse-roughness-across-scales-w-jackknife.R
@@ -10,172 +10,105 @@ set.seed(1234)
 
 # Test
 if (FALSE) {
-  result <- list(SWAFR_variables[[1]], GCFR_variables[[1]]) %>%
-    map(~ prep_layer2(.x, resolution = 0.25))
-      x = ..1, y = ..2,
-      resolution = 0.25,
-      n = 1, size = 500
-    )
-  )
-  result %>%
-    map(bind_rows) %>%
-    map(summarise_if,
-        is.numeric,
-        .funs = list(mean = mean, sd = sd)) %>%
-    bind_rows(.id = "variable")
+  list(GCFR_variables_3QDS[[1]], SWAFR_variables_3QDS[[1]]) %>%
+    map(prep_layer2) %>%
+    pairwise_matrix() %>%
+    pairwise_compare() %>%
+    CLES_jackknife(n = 100, size_x = 10, size_y = 10)
   # Works!
 }
 
-# Run on QDS, HDS, 3QDS
-resolutions <- c(0.05, 0.25, 0.50, 0.75)
-bootstrap_results <- vector("list", length = length(resolutions))
-for (i in 2:4) {  # Doing 0.05º separately, using disc, below
-  result <- pmap(
-    .l = list(
-      ..1 = SWAFR_variables,
-      ..2 = GCFR_variables,
-      ..3 = var_names
-    ),
-    .f = ~ compare_roughness_bootstrapped(
-      x = ..1, x_region_name = "SWAFR",
-      y = ..2, y_region_name = "GCFR",
-      variable = ..3, resolution = resolutions[[i]],
-      n_samples = 1000,
-      use_disc = TRUE  # These can all fit in memory, but using disc just in case
-    )
-  )
-  bootstrap_results[[i]] <- result %>%
-    map(bind_rows) %>%
-    map(summarise_if,
-        is.numeric,
-        .funs = list(mean = mean, sd = sd)) %>%
-    bind_rows(.id = "variable")
-}
-#names(bootstrap_results) <- c("0.05º", "QDS", "HDS", "3QDS")
-names(bootstrap_results) <- c("QDS", "HDS", "3QDS")
-bootstrap_results %<>% bind_rows(.id = "resolution")
+# Compute and store all pairwise comparisons of roughness in cells -------------
 
-pos <- position_dodge(0.15)
-ggplot(bootstrap_results,
-       aes(resolution, CLES_mean,
-           col = variable)) +
-  geom_point(position = pos) +
-  geom_linerange(aes(ymin = CLES_mean - CLES_sd,
-                     ymax = CLES_mean + CLES_sd),
-                 position = pos) +
-  geom_line(aes(group = variable), position = pos)
-
-# Do for 0.05 "manually", as is an onerous computation
-# TODO: Re-run this, as my first run was before I fixed the mistake
-#       where bootstrap_sample() returned the matrix sideways (stupid default)
-result_0.05 <- pmap(
-  .l = list(
-    ..1 = SWAFR_variables,
-    ..2 = GCFR_variables,
-    ..3 = var_names
-  ),
-  .f = ~ compare_roughness_bootstrapped(
-    x = ..1, x_region_name = "SWAFR",
-    y = ..2, y_region_name = "GCFR",
-    variable = ..3, resolution = resolutions[[1]],
-    n_samples = 1000,
-    use_disc = TRUE
-  )
+# QDS
+pw_comparisons_QDS <- map2(
+  .x = GCFR_variables_QDS,
+  .y = SWAFR_variables_QDS,
+  .f = ~
+    list(.x, .y) %>%
+    map(prep_layer2) %>%
+    pairwise_matrix() %>%
+    pairwise_compare()
 )
-bootstrap_results[[1]] <- result_0.05 %>%
-  map(bind_rows) %>%
-  map(summarise_if,
-      is.numeric,
-      .funs = list(mean = mean, sd = sd)) %>%
-  bind_rows(.id = "variable")
-bootstrap_results_0.05 <- result_0.05 %>%
-  map(bind_rows) %>%
-  map(summarise_if,
-      is.numeric,
-      .funs = list(mean = mean, sd = sd)) %>%
-  bind_rows(.id = "variable")
-
-#! Old script continues here ---------------------------------------------------
-
-# Save to disc
-write_csv(
-  test_results_summary,
-  here::here("outputs/04_roughness-across-scales/test_results_summary.csv")
+# HDS
+pw_comparisons_HDS <- map2(
+  .x = GCFR_variables_HDS,
+  .y = SWAFR_variables_HDS,
+  .f = ~
+    list(.x, .y) %>%
+    map(prep_layer2) %>%
+    pairwise_matrix() %>%
+    pairwise_compare()
 )
-
-# CLES for those tests ---------------------------------------------------------
-
-# <https://en.wikipedia.org/wiki/Mann%E2%80%93Whitney_U_test#Effect_sizes>
-test_results_CLES <- foreach(resolution = list(0.05, 0.25, 0.50, 0.75)) %do% {
-  out <- foreach(GCFR = GCFR_variables, SWAFR = SWAFR_variables) %do% {
-    canprot::CLES(
-      na.omit(prep_layer(GCFR)[]),
-      na.omit(prep_layer(SWAFR)[])
-    )
-  }
-  names(out) <- var_names
-  out
-}
-names(test_results_CLES) <- c("0.05º", "QDS", "HDS", "3QDS")
-
-test_results_CLES_for_plot <- test_results_CLES %>%
-  map(as_tibble) %$%
-  rbind(.$`0.05º`, .$QDS, .$HDS, .$`3QDS`) %>%
-  cbind(resolution = names(test_results_CLES), .) %>%
-  gather(variable, CLES, -resolution) %>%
-  as_tibble()
-
-# Save to disc
-write_csv(
-  test_results_CLES_for_plot,
-  here::here("outputs/04_roughness-across-scales/test_results_CLES_for_plot.csv")
+# 3QDS
+pw_comparisons_3QDS <- map2(
+  .x = GCFR_variables_3QDS,
+  .y = SWAFR_variables_3QDS,
+  .f = ~
+    list(.x, .y) %>%
+    map(prep_layer2) %>%
+    pairwise_matrix() %>%
+    pairwise_compare()
 )
+# 0.05º
+# TODO
+# NOTE: do NOT put before QDS, as set.seed affects commands in order
+#pw_comparisons_0.05 <- map2(
+#  .x = GCFR_variables,
+#  .y = SWAFR_variables,
+#  .f = ~
+#    list(.x, .y) %>%
+#    map(prep_layer2) %>%
+#    pairwise_matrix() %>%
+#    pairwise_compare()
+#)
 
-# Prep data for violin plot ----------------------------------------------------
+# Jackknife-sample those and get CLES for each jackknife-sample ----------------
 
-data_for_violin_plot <- foreach(resolution = list(0.05, 0.25, 0.50, 0.75)) %do% {
-  rbind(
-    cbind(region = "GCFR", map2_df(GCFR_variables, resolution, prep_layer)),
-    cbind(region = "SWAFR", map2_df(SWAFR_variables, resolution, prep_layer))
-  )
-}
-data_for_violin_plot_tidy <- data_for_violin_plot %$%
-  rbind(cbind(resolution = "0.05º", .[[1]]),
-        cbind(resolution = "QDS",   .[[2]]),
-        cbind(resolution = "HDS",   .[[3]]),
-        cbind(resolution = "3QDS",  .[[4]])) %>%
-  as_tibble() %>%
-  gather(variable, roughness, -resolution, -region) %>%
-  na.omit() %>%
-  group_by(resolution, variable) %>%
-  mutate(z_roughness = scale(roughness)) %>%  # Z-scale!
-  ungroup() %>%
-  mutate(variable = factor(variable, levels = var_names),
-         region = ifelse(region == "GCFR", "Cape", "SWA"))
+# (And not for 3QDS as that is the limiting scale)
 
-# Save to disc
-write_csv(
-  data_for_violin_plot_tidy,
-  here::here("outputs/04_roughness-across-scales/data_for_violin_plot.csv")
+n_jackknifes <- 1000
+nrow_3QDS <- unique(map_int(pw_comparisons_3QDS, nrow))
+ncol_3QDS <- unique(map_int(pw_comparisons_3QDS, ncol))
+
+# QDS
+jackknifed_results_QDS <- map_df(pw_comparisons_QDS,
+  CLES_jackknife,
+  n = n_jackknifes,
+  size_x = nrow_3QDS,
+  size_y = ncol_3QDS
 )
-
-# Analyse IQ95R (and IQ99R) ~ scale --------------------------------------------
-
-IQ95R_data <- data_for_violin_plot_tidy %>%
-  group_by(resolution, region, variable) %>%
-  summarise(IQ99R = IQ99R(z_roughness),
-            IQ95R = IQ95R(z_roughness)) %>%
-  gather(quantile, IXR,
-         -resolution, -region, -variable) %>%
-  mutate(quantile = ifelse(quantile == "IQ99R",
-                           0.99,
-                           ifelse(quantile == "IQ95R",
-                                  0.95,
-                                  NA))) %>%
-  ungroup()
-
-# Save to disc
-write_csv(
-  IQ95R_data,
-  here::here("outputs/04_roughness-across-scales/IQ95R_data.csv")
+# HDS
+jackknifed_results_HDS <- map_df(pw_comparisons_HDS,
+  CLES_jackknife,
+  n = n_jackknifes,
+  size_x = nrow_3QDS,
+  size_y = ncol_3QDS
 )
+# 0.05º
+# TODO
+# NOTE: do NOT put before QDS, as set.seed affects commands in order
+#jackknifed_results_0.05 <- map_df(pw_comparisons_0.05,
+#  CLES_jackknife,
+#  n = n_jackknifes,
+#  size_x = nrow_3QDS,
+#  size_y = ncol_3QDS
+#)
+
+# Summarise the jackknifed CLES values -----------------------------------------
+
+jackknifed_results_summary_QDS <- summarise_all(
+  jackknifed_results_QDS,
+  .funs = list(mean = mean, sd = sd)
+)
+jackknifed_results_summary_HDS <- summarise_all(
+  jackknifed_results_HDS,
+  .funs = list(mean = mean, sd = sd)
+)
+# 0.05º
+# TODO
+# NOTE: do NOT put before QDS, as set.seed affects commands in order
+#jackknifed_results_summary_0.05 <- summarise_all(
+#  jackknifed_results_0.05,
+#  .funs = list(mean = mean, sd = sd)
+#)
