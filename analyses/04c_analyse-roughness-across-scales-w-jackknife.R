dd445a436f5e81a6bc1ee6e0611255cbdbf072c2
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-08-02 20:09:11 +0200

    Init setup and code to be run for 0.05 scale pw matrices
    To be run now now

--- analyses/04c_analyse-roughness-across-scales-w-jackknife.R
+++ analyses/04c_analyse-roughness-across-scales-w-jackknife.R
@@ -12,7 +12,156 @@ set.seed(1234)
 
 # Compute and store all pairwise comparisons of roughness in cells -------------
 
-# QDS
+# .... 0.05 --------------------------------------------------------------------
+
+# Let's plan how long this should take
+if (FALSE) {
+
+  n_GCFR_cells <- length(prep_layer2(GCFR_variables[[1]]))
+  n_SWAFR_cells <- length(prep_layer2(SWAFR_variables[[1]]))
+  print(glue(
+    "There are {n_GCFR_cells} x {n_SWAFR_cells} = \\
+    {n_GCFR_cells * n_SWAFR_cells} comparisons \\
+    to do at the 0.05 x 0.05 degree scale"
+  ))
+  n <- 10
+  size <- 1000
+  pw_benchmark_1e9 <- microbenchmark(times = n, {
+    pairwise_compare(matrix(
+      nrow = size, ncol = size,
+      dimnames = list(seq(size), seq(size))
+    ))
+  })
+  time_bench_comparisons <- mean(pw_benchmark_1e9$time / 1e9)  # Convert to s
+  time_GCFR_SWAFR_comparisons <-
+    (n_GCFR_cells * n_SWAFR_cells) %>%
+    divide_by(size ^ 2) %>%  # Convert to millions
+    multiply_by(time_bench_comparisons) %>%  # Time in s
+    divide_by(60)  # Convert to minutes
+  print(glue(
+    "Linearly running 1000000 comparisons takes ca. \\
+    {format(time_bench_comparisons, digits = 4)} seconds.
+    Thus, running {n_GCFR_cells * n_SWAFR_cells} comparisons should take ca. \\
+    {format(time_GCFR_SWAFR_comparisons, digits = 4)} minutes.
+    For 9 variables, this should take at least \\
+    {format((9 * time_GCFR_SWAFR_comparisons) / 60, digits = 4)} hours total.
+    Because things rarely go *that* well, \\
+    let's be safe and leave 6 to 8 hours for this."
+  ))
+
+}
+
+
+# Done:
+parallel_elev_pw <-
+  list(GCFR_variables[[1]], SWAFR_variables[[1]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_elev_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_Elevation_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_elev_pw)
+
+# TODO:
+parallel_MAP_pw <-
+  list(GCFR_variables[[2]], SWAFR_variables[[2]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_MAP_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_MAP_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_MAP_pw)
+
+# TODO:
+parallel_PDQ_pw <-
+  list(GCFR_variables[[3]], SWAFR_variables[[3]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_PDQ_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_PDQ_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_PDQ_pw)
+
+# TODO:
+parallel_surfT_pw <-
+  list(GCFR_variables[[4]], SWAFR_variables[[4]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_surfT_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_Surface-T_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_surfT_pw)
+
+# TODO:
+parallel_NDVI_pw <-
+  list(GCFR_variables[[5]], SWAFR_variables[[5]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_NDVI_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_NDVI_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_NDVI_pw)
+
+# TODO:
+parallel_CEC_pw <-
+  list(GCFR_variables[[6]], SWAFR_variables[[6]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_CEC_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_CEC_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_CEC_pw)
+
+# TODO:
+parallel_clay_pw <-
+  list(GCFR_variables[[7]], SWAFR_variables[[7]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_clay_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_Clay_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_clay_pw)
+
+# TODO:
+parallel_soilC_pw <-
+  list(GCFR_variables[[8]], SWAFR_variables[[8]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_soilC_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_Soil-C_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_soilC_pw)
+
+# TODO:
+parallel_pH_pw <-
+  list(GCFR_variables[[9]], SWAFR_variables[[9]]) %>%
+  map(prep_layer2) %>%
+  pairwise_matrix() %>%
+  pairwise_compare(use_parallel = TRUE)
+write_csv(
+  as.data.frame(parallel_pH_pw),
+  here::here("outputs/04_roughness-across-scales/pw-comparisons_pH_0.05_parallel_2018-08-02.csv")
+)
+rm(parallel_pH_pw)
+
+# .... QDS ---------------------------------------------------------------------
+
 pw_comparisons_QDS <- map2(
   .x = GCFR_variables_QDS,
   .y = SWAFR_variables_QDS,
@@ -22,7 +171,9 @@ pw_comparisons_QDS <- map2(
     pairwise_matrix() %>%
     pairwise_compare()
 )
-# HDS
+
+# .... HDS ---------------------------------------------------------------------
+
 pw_comparisons_HDS <- map2(
   .x = GCFR_variables_HDS,
   .y = SWAFR_variables_HDS,
@@ -32,7 +183,9 @@ pw_comparisons_HDS <- map2(
     pairwise_matrix() %>%
     pairwise_compare()
 )
-# 3QDS
+
+# .... 3QDS --------------------------------------------------------------------
+
 pw_comparisons_3QDS <- map2(
   .x = GCFR_variables_3QDS,
   .y = SWAFR_variables_3QDS,
@@ -42,18 +195,6 @@ pw_comparisons_3QDS <- map2(
     pairwise_matrix() %>%
     pairwise_compare()
 )
-# 0.05º
-# TODO
-# NOTE: do NOT put before QDS, as set.seed affects commands in order
-#pw_comparisons_0.05 <- map2(
-#  .x = GCFR_variables,
-#  .y = SWAFR_variables,
-#  .f = ~
-#    list(.x, .y) %>%
-#    map(prep_layer2) %>%
-#    pairwise_matrix() %>%
-#    pairwise_compare()
-#)
 
 # Jackknife-sample those and get CLES for each jackknife-sample ----------------
 
