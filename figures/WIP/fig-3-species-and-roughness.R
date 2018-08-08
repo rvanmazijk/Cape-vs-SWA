b7e86a23a56953d165ab4f7bf772d360890302be
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-08-08 14:52:22 +0200

    Source for SDF plots of GWR models -> WIP/

--- figures/WIP/fig-3-species-and-roughness.R
+++ figures/WIP/fig-3-species-and-roughness.R
@@ -12,18 +12,7 @@ source(here::here("figures/figure-setup.R"))
 
 # Visualise model results ------------------------------------------------------
 
-#### WIP
-plot(Elevation_se ~ GCFR_all_QDS_pts@data$Elevation, data = GCFR_models$elev$SDF@data)
-spplot(
-  GCFR_models$elev$SDF["Elevation"], scales = list(draw = TRUE),
-  sp.layout = list(GCFR_border_buffered)
 )
-spplot(
-  SWAFR_models$elev$SDF["Elevation"], scales = list(draw = TRUE),
-  sp.layout = list(SWAFR_border_buffered)
-)
-####
-
 # .... Combined regions' models ------------------------------------------------
 
 full_coeff <- combined_models$full$SDF@data
