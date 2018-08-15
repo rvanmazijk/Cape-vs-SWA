6f55e75bfa81672f5786275364d77ca408372c4b
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-08-15 14:06:25 +0200

    More of floral data compilation and cleaning -> data/ import scripts

--- analyses/02_analyse-turnover.R
+++ analyses/02_analyse-turnover.R
@@ -4,47 +4,9 @@
 
 source(here::here("setup.R"))
 #source(here::here("data/01_import-region-polygons.R"))
-#source(here::here("data/02_import-floral-data.R"))
+source(here::here("data/02_import-floral-data.R"))
 
-# Compile data -----------------------------------------------------------------
-
-GCFR_spp <- readOGR(here::here(
-  "outputs/species-turnover-and-richness/GCFR_spp_2018-08-14"
-))
-SWAFR_spp <- readOGR(here::here(
-  "outputs/species-turnover-and-richness/SWAFR_spp_2018-08-14"
-))
-
-vars_to_keep <- c(
-  "HDS_richness",
-  "n_QDS",
-  "mean_QDS_richness",
-  "mean_QDS_jaccard"
-)
-names(GCFR_spp)[6:9] <- vars_to_keep
-names(SWAFR_spp)[6:9] <- vars_to_keep
-
-GCFR_spp_data <- GCFR_spp@data %>%
-  select(vars_to_keep) %>%
-  distinct()
-SWAFR_spp_data <- SWAFR_spp@data %>%
-  select(vars_to_keep) %>%
-  distinct()
-
-richness_turnover_data <-
-  rbind(
-    cbind(region = "Cape", GCFR_spp_data),
-    cbind(region = "SWA", SWAFR_spp_data)
-  ) %>%
-  as_tibble() %>%
-  filter(n_QDS > 1) %>%  # turnover is non-sensicle for 1 QDS)
-  mutate(
-    add_residual_turnover = HDS_richness - mean_QDS_richness,
-    add_residual_turnover_prop = add_residual_turnover / HDS_richness,
-    mul_residual_turnover = HDS_richness / mean_QDS_richness
-  )
-
-# Compare data between GCFR and SWAFR ------------------------------------------
+# Compare turnover between GCFR and SWAFR --------------------------------------
 
 # .... Mean Jaccard distance between QDS in HDS --------------------------------
 
--- data/02_import-floral-data.R
+++ data/02_import-floral-data.R
@@ -30,3 +30,35 @@ GCFR_spp <- readOGR(glue(
 SWAFR_spp <- readOGR(glue(
   "{flora_dir}/SWAFR_spp_2018-08-14"
 ))
+
+vars_to_keep <- c(
+  "hdgc",
+  "HDS_richness",
+  "n_QDS",
+  "mean_QDS_richness",
+  "mean_QDS_jaccard"
+)
+names(GCFR_spp)[5:9] <- vars_to_keep
+names(SWAFR_spp)[5:9] <- vars_to_keep
+
+GCFR_spp_data <- GCFR_spp@data %>%
+  select(vars_to_keep) %>%
+  filter(n_QDS > 1) %>%
+  distinct()
+SWAFR_spp_data <- SWAFR_spp@data %>%
+  select(vars_to_keep) %>%
+  filter(n_QDS > 1) %>%
+  distinct()
+
+richness_turnover_data <-
+  rbind(
+    cbind(region = "Cape", GCFR_spp_data),
+    cbind(region = "SWA", SWAFR_spp_data)
+  ) %>%
+  as_tibble() %>%
+  filter(n_QDS > 1) %>%  # turnover is non-sensicle for 1 QDS)
+  mutate(
+    add_residual_turnover = HDS_richness - mean_QDS_richness,
+    add_residual_turnover_prop = add_residual_turnover / HDS_richness,
+    mul_residual_turnover = HDS_richness / mean_QDS_richness
+  )
