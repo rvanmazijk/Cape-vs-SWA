822eaeee4fb1f7ae5819d1f0eb86ef6b1da3622b
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-08-16 10:16:11 +0200

    Remove analysis numbering, rename many scripts, remove some old functions

---
+++ functions/stack_soils.R
@@ -0,0 +1,20 @@
+# Create a RasterStack of all the soil variables together
+stack_soils <- function(region = c("GCFR", "SWAFR"),
+                        variables = c("CECSOL",
+                                      "BLDFIE",
+                                      "CLYPPT",
+                                      "CRFVOL",
+                                      "OCDENS",
+                                      "PHIKCL",
+                                      "SLTPPT",
+                                      "SNDPPT")) {
+  soils <- raster::stack()
+  for (variable in variables) {
+    x <- raster::raster(here::here(
+      "data/derived-data/soils/",
+      glue("{region}_{region}_{variable}_M_250m_std_CRS_0.05_0.05.tif")
+    ))
+    soils <- raster::stack(soils, x)
+  }
+  soils
+}
