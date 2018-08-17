7d911da246644b8d86ea1c74c1306c228d73758a
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-08-17 18:20:52 +0200

    [WIP]

--- analyses/analyse-species-and-roughness.R
+++ analyses/analyse-species-and-roughness.R
@@ -144,27 +144,29 @@ all_data_HDS <- map2(
     .x <- .x[.x$hdgc %in% final_hdgcs, ]
     .y <- .y[.y$hdgc %in% final_hdgcs, ]
 
-    # Init empty columns in the environmental data
-    .y$region <- NULL
-    .y$HDS_richness <- NULL
-    .y$n_QDS <- NULL
-    .y$mean_QDS_richness <- NULL
-    .y$mean_QDS_jaccard <- NULL
-    .y$add_residual_turnover <- NULL
-    .y$add_residual_turnover_prop <- NULL
-    .y$mul_residual_turnover <- NULL
-
-    # Add richness-turnover data
-    .y$region <- .x$region
-    .y$HDS_richness <- .x$HDS_richness
-    .y$n_QDS <- .x$n_QDS
-    .y$mean_QDS_richness <- .x$mean_QDS_richness
-    .y$mean_QDS_jaccard <- .x$mean_QDS_jaccard
-    .y$add_residual_turnover <- .x$add_residual_turnover
-    .y$add_residual_turnover_prop <- .x$add_residual_turnover_prop
-    .y$mul_residual_turnover <- .x$mul_residual_turnover
-
+    .y@data <- right_join(.y@data, .x)
     .y
+    ## Init empty columns in the environmental data
+    #.y$region <- NULL
+    #.y$HDS_richness <- NULL
+    #.y$n_QDS <- NULL
+    #.y$mean_QDS_richness <- NULL
+    #.y$mean_QDS_jaccard <- NULL
+    #.y$add_residual_turnover <- NULL
+    #.y$add_residual_turnover_prop <- NULL
+    #.y$mul_residual_turnover <- NULL
+
+    ## Add richness-turnover data
+    #.y$region <- .x$region
+    #.y$HDS_richness <- .x$HDS_richness
+    #.y$n_QDS <- .x$n_QDS
+    #.y$mean_QDS_richness <- .x$mean_QDS_richness
+    #.y$mean_QDS_jaccard <- .x$mean_QDS_jaccard
+    #.y$add_residual_turnover <- .x$add_residual_turnover
+    #.y$add_residual_turnover_prop <- .x$add_residual_turnover_prop
+    #.y$mul_residual_turnover <- .x$mul_residual_turnover
+
+    #.y
 
   }
 )
@@ -233,234 +235,88 @@ gwr_model2 <- function(my_data, response, predictors) {
   model_gwr
 }
 names(all_data_HDS)
-gwr_model2(my_data = all_data_HDS, "HDS_richness", 1)
-gwr_model2(
-  my_data = all_data_HDS,
-  "HDS_richness",
-  names(all_data_HDS)[str_detect(names(all_data_HDS), "rough_")]
-)
 
-#### OLD MODELS BELOW ####
+# Practice
+if (FALSE) {
+  gwr_model2(my_data = all_data_HDS, "log(HDS_richness)", 1)
+  gwr_model2(
+    my_data = all_data_HDS,
+    "log(HDS_richness)",
+    names(all_data_HDS)[str_detect(names(all_data_HDS), "rough_")]
+  )
+}
+# Works :)
 
 # .... Separate regions' models ------------------------------------------------
 
-model_specs <- list(
-  null     =  c(NULL),
-  abs      =  c(1, 2:10),
-  rough    =  c(1, 11:19),
-  elev     =  c(1, 2, 11),
-  non_elev = -c(2, 11),
-  soil     =  c(1, 7:10, 16:19),
-  non_soil = -c(7:10, 16:19),
-  full     =  c("all")
+# Without the region or HDS-geocode column
+Cape_data_HDS <- all_data_HDS[all_data_HDS$region == "Cape", -c(1, 2)]
+SWA_data_HDS <- all_data_HDS[all_data_HDS$region == "SWA", -c(1, 2)]
+
+# Describe different sets of predictor variables
+abs_var_names <- c(
+  "Elevation", "MAP", "PDQ", "Surface.T", "NDVI",
+  "CEC", "Clay", "Soil.C", "pH"
+)
+rough_var_names <- paste0("rough_", abs_var_names)
+richness_model_specs <- list(
+  null     =  1,
+  abs      = abs_var_names,
+  rough    = rough_var_names,
+  elev     = c("Elevation", "rough_Elevation"),
+  non_elev = c(abs_var_names[-1], rough_var_names[-1]),
+  soil     = c(abs_var_names[6:9], rough_var_names[6:9]),
+  non_soil = c(abs_var_names[1:5], rough_var_names[1:5]),
+  full     = c(abs_var_names, rough_var_names)
 )
 
-# Fit models if not already done
-
-GCFR_models_path <- glue("{out_dir}/GCFR_models.RDS")
-if (!file.exists(GCFR_models_path)) {
-  set.seed(1234)
-  GCFR_models <- map(.x = model_specs,
-    .f = ~ gwr_model(
-      data = GCFR_all_QDS_pts,
-      columns = .x,
-      rasterize_with = GCFR_richness_QDS
-    )
+set.seed(1234)
+Cape_HDS_richness_models <- map(
+  .x = richness_model_specs,
+  .f = ~ gwr_model2(
+    my_data = Cape_data_HDS,
+    response = "log(HDS_richness)",
+    predictors = .x
   )
-  write_rds(GCFR_models, GCFR_models_path)
-}
-GCFR_models <- read_rds(GCFR_models_path)
+)
+Cape_models_summary <- delta_AICc(Cape_HDS_richness_models)
+best_Cape_model <- Cape_models_summary$model[Cape_models_summary$delta_AICc == 0]
+plot(
+  x = log(Cape_data_HDS$HDS_richness),
+  y = Cape_HDS_richness_models[[best_model]]$SDF$pred
+)
+abline(0, 1)
 
-SWAFR_models_path <- glue("{out_dir}/SWAFR_models.RDS")
-if (!file.exists(SWAFR_models_path)) {
-  set.seed(1234)
-  SWAFR_models <- map(.x = model_specs,
-    .f = ~ gwr_model(
-      data = SWAFR_all_QDS_pts,
-      columns = .x,
-      rasterize_with = SWAFR_richness_QDS
-    )
+set.seed(1234)
+SWA_HDS_richness_models <- map(
+  .x = richness_model_specs,
+  .f = ~ gwr_model2(
+    my_data = SWA_data_HDS,
+    response = "log(HDS_richness)",
+    predictors = .x
   )
-  write_rds(SWAFR_models, SWAFR_models_path)
-}
-SWAFR_models <- read_rds(SWAFR_models_path)
-
-# NOTE:
-# - non_elev: richness ~ soil + climate + ndvi + roughnesses thereof
-# - non_soil: richness ~ elev + climate + ndvi + roughnesses thereof
-# - etc.
-
-# .... Combined regions' models ------------------------------------------------
-
-# Note, "region" column is simply to aid visualisation later.
-# GWR cannot actually handle a discrete term directly (hence -1 for
-# some model specs) but the spatial covariance matrix means that we can see
-# how different the local coefficients turn out to be estimated w/i each region.
-
-model_specs <- list(
-  null     =  c(NULL),
-  abs      =  c(2, 3:11),
-  rough    =  c(2, 12:20),
-  elev     =  c(2, 3, 12),
-  non_elev = -c(1, 3, 12),
-  soil     =  c(2, 8:11, 17:20),
-  non_soil = -c(1, 8:11, 17:20),
-  full     = -c(1)
 )
-
-# Fit models if not already done
-
-combined_models_path <- glue("{out_dir}/combined_models.RDS")
-if (!file.exists(combined_models_path)) {
-  set.seed(1234)
-  foo <- map(.x = model_specs,
-    .f = ~ gwr_model(
-      data = BOTH_all_QDS_pts,
-      columns = .x
-    )
-  )
-  write_rds(combined_models, combined_models_path)
-} else {
-  combined_models <- read_rds(combined_models_path)
-}
-
-# Interpret models -------------------------------------------------------------
-
-# .... Separate regions' models ------------------------------------------------
-
-# Overall "ranking" of model fits
-delta_AICc(
-  GCFR_models,
-  glue("{out_dir}/GCFR_models_AICc_all.csv")
+SWA_models_summary <- delta_AICc(SWA_HDS_richness_models)
+best_SWA_model <- SWA_models_summary$model[SWA_models_summary$delta_AICc == 0]
+plot(
+  x = log(SWA_data_HDS$HDS_richness),
+  y = SWA_HDS_richness_models[[best_SWA_model]]$SDF$pred
 )
-delta_AICc(
-  SWAFR_models,
-  glue("{out_dir}/SWAFR_models_AICc_all.csv")
+abline(0, 1)
+# Why does ELEVATION matter in SWA??
+plot(
+  x = SWA_data_HDS$Elevation,
+  y = SWA_HDS_richness_models[[best_SWA_model]]$SDF$pred
 )
-
-# Specific model comparisons
-abs_vs_rough     <- c("null", "abs",  "rough",    "full")
-elev_vs_non_elev <- c("null", "elev", "non_elev", "full")
-soil_vs_non_soil <- c("null", "soil", "non_soil", "full")
-delta_AICc(
-  GCFR_models[abs_vs_rough],
-  glue("{out_dir}/GCFR_models_AICc_abs_vs_rough.csv")
+plot(
+  x = SWA_data_HDS$rough_Elevation,
+  y = SWA_HDS_richness_models[[best_SWA_model]]$SDF$pred
 )
-delta_AICc(
-  SWAFR_models[abs_vs_rough],
-  glue("{out_dir}/SWAFR_models_AICc_abs_vs_rough.csv")
+plot(
+  x = SWA_data_HDS$Elevation,
+  y = SWA_data_HDS$HDS_richness
 )
-delta_AICc(
-  GCFR_models[elev_vs_non_elev],
-  glue("{out_dir}/GCFR_models_AICc_elev_vs_non_elev.csv")
-)
-delta_AICc(
-  SWAFR_models[elev_vs_non_elev],
-  glue("{out_dir}/SWAFR_models_AICc_elev_vs_non_elev.csv")
-)
-delta_AICc(
-  GCFR_models[soil_vs_non_soil],
-  glue("{out_dir}/GCFR_models_AICc_soil_vs_non_soil.csv")
-)
-delta_AICc(
-  SWAFR_models[soil_vs_non_soil],
-  glue("{out_dir}/SWAFR_models_AICc_soil_vs_non_soil.csv")
-)
-
-# .... Combined regions' models ------------------------------------------------
-
-delta_AICc(
-  combined_models,
-  glue("{out_dir}/combined_models_AICc_all.csv")
-)
-delta_AICc(
-  combined_models[abs_vs_rough],
-  glue("{out_dir}/combined_models_AICc_abs_vs_rough.csv")
-)
-delta_AICc(
-  combined_models[elev_vs_non_elev],
-  glue("{out_dir}/combined_models_AICc_elev_vs_non_elev.csv")
-)
-delta_AICc(
-  combined_models[soil_vs_non_soil],
-  glue("{out_dir}/combined_models_AICc_soil_vs_non_soil.csv")
-)
-
-# Output all model outputs -----------------------------------------------------
-
-GCFR_models_outputs <- map_df(
-  .x = GCFR_models,
-  .id = "model",
-  .f = function(.x) {
-    model_outputs <- .x$SDF@data
-    model_outputs <- cbind(
-      obs_log_richness = GCFR_all_QDS_pts@data$richness,
-      model_outputs
-    )
-    names(model_outputs)[names(model_outputs) == "pred.se"][[2]] <- "pred.se2"
-    model_outputs %<>%
-      as_tibble() %>%
-      mutate(
-        obs_richness = exp(obs_log_richness),
-        pred_richness = exp(pred)
-      )
-  }
-)
-GCFR_models_outputs$region <- "GCFR"
-
-SWAFR_models_outputs <- map_df(
-  .x = SWAFR_models,
-  .id = "model",
-  .f = function(.x) {
-    model_outputs <- .x$SDF@data
-    model_outputs <- cbind(
-      obs_log_richness = SWAFR_all_QDS_pts@data$richness,
-      model_outputs
-    )
-    names(model_outputs)[names(model_outputs) == "pred.se"][[2]] <- "pred.se2"
-    model_outputs %<>%
-      as_tibble() %>%
-      mutate(
-        obs_richness = exp(obs_log_richness),
-        pred_richness = exp(pred)
-      )
-  }
-)
-SWAFR_models_outputs$region <- "SWAFR"
-
-combined_models_outputs <- map_df(
-  .x = combined_models,
-  .id = "model",
-  .f = function(.x) {
-    model_outputs <- .x$SDF@data
-    model_outputs <- cbind(
-      obs_log_richness = BOTH_all_QDS_pts@data$richness,
-      region = BOTH_all_QDS_pts@data$region,
-      model_outputs
-    )
-    names(model_outputs)[names(model_outputs) == "pred.se"][[2]] <- "pred.se2"
-    model_outputs %<>%
-      as_tibble() %>%
-      mutate(
-        region = case_when(
-          region == "GCFR" ~ "Cape",
-          region == "SWAFR" ~ "SWA"
-        ),
-        obs_richness = exp(obs_log_richness),
-        pred_richness = exp(pred)
-      )
-  }
-)
-
-all_GWR_models_outputs <- bind_rows(
-  list(
-    GCFR = GCFR_models_outputs,
-    SWAFR = SWAFR_models_outputs,
-    both = combined_models_outputs
-  ),
-  .id = "model_region"
-)
-write_csv(
-  all_GWR_models_outputs,
-  glue("{out_dir}/all_GWR_models_outputs.csv")
+plot(
+  x = SWA_data_HDS$rough_Elevation,
+  y = SWA_data_HDS$HDS_richness
 )
