a584925c5f0dbf2cd540114a9ef785d64ca39319
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-08-29 17:21:56 +0200

    Renumber and reorganise figure-scripts/

--- R/figure-scripts/repo-structure-graphs/make-repo-structure-graphs.R
+++
@@ -1,8 +0,0 @@
-grViz2svg <- function(file) {
-  graph <- DiagrammeR::grViz(file)
-  graph <- DiagrammeRsvg::export_svg(graph)
-  write(graph, paste0(xfun::sans_ext(file), ".svg"))
-}
-
-grViz2svg(here::here("docs/repo-structure.gv"))
-grViz2svg(here::here("docs/repo-structure-detailed.gv"))
