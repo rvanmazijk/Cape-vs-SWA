74a9213d51242def7200b992d782df16ff83f4b4
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-08-31 16:35:12 +0200

    Update early result exploration code

--- R/02_analyses/analyse-species-environment-relationships/04_explore-predicted-vs-observed.R
+++ R/02_analyses/analyse-species-environment-relationships/04_explore-predicted-vs-observed.R
@@ -4,46 +4,77 @@
 # Cape vs SWA publication
 # Ruan van Mazijk
 
-GCFR_pred_vs_obs_data <-
-  tibble(
-    pred = GCFR_gbm_step_simp$fitted,
-    obs = GCFR_gbm_step_simp$data$y
-  ) %>%
-  mutate(
-    exp_pred = exp(pred),
-    exp_obs = exp(obs)
+library(visreg)
+
+# Extract predicted and observed richness and turnover from BRTs ---------------
+
+BRT_pred_vs_obs_models <- map(
+  # For each response:
+  .x = gbm_steps_simp,
+  .f = ~ map(
+    # For each region:
+    .x = .x,
+    .f = function(.x) {
+      pred_obs_data <- .x %$%
+        tibble(
+          pred = fitted,
+          obs = data$y
+        ) %>%
+        mutate(
+          exp_pred = exp(pred),
+          exp_obs = exp(obs)
+        )
+      pred_obs_m <- lm(pred ~ obs, data = pred_obs_data)
+      pred_obs_m_exp <- lm(exp_pred ~ exp_obs, data = pred_obs_data)
+      list(
+        pred_obs_m = pred_obs_m,
+        pred_obs_m_exp = pred_obs_m_exp
+      )
+    }
   )
-SWAFR_pred_vs_obs_data <-
-  tibble(
-    pred = SWAFR_gbm_step_simp$fitted,
-    obs = SWAFR_gbm_step_simp$data$y
-  ) %>%
-  mutate(
-    exp_pred = exp(pred),
-    exp_obs = exp(obs)
+)
+
+# Get all R^2 values -----------------------------------------------------------
+
+BRT_pred_vs_obs_r2 <- map(
+  # For each response:
+  .x = BRT_pred_vs_obs_models,
+  .f = ~ map(
+    # For each region:
+    .x = .x,
+    .f = ~ map(
+      # For the log and linear models:
+      .x = .x,
+      .f = ~ glance(.x) %>%
+        select(r.squared)
+    )
   )
+)
 
-GCFR_pred_vs_obs_m <- lm(pred ~ obs, data = GCFR_pred_vs_obs_data)
-GCFR_pred_vs_obs_m_exp <- lm(exp_pred ~ exp_obs, data = GCFR_pred_vs_obs_data)
-SWAFR_pred_vs_obs_m <- lm(pred ~ obs, data = SWAFR_pred_vs_obs_data)
-SWAFR_pred_vs_obs_m_exp <- lm(exp_pred ~ exp_obs, data = SWAFR_pred_vs_obs_data)
+# Generate visreg ggplots ------------------------------------------------------
 
-tidy(GCFR_pred_vs_obs_m)
-tidy(GCFR_pred_vs_obs_m_exp)
-tidy(SWAFR_pred_vs_obs_m)
-tidy(SWAFR_pred_vs_obs_m_exp)
+BRT_pred_vs_obs_plots <- map(
+  # For each response:
+  .x = BRT_pred_vs_obs_models,
+  .f = ~ map(
+    # For each region:
+    .x = .x,
+    .f = ~ map(
+      # For the log and linear models:
+      .x = .x,
+      .f = function(.x) {
+        visreg(.x, gg = TRUE) +
+          geom_abline(intercept = 0, slope = 1, linetype = "dashed")# +
+          annotate(
+            "text",
+            x = 0.25 * max(.x$model[[2]]),
+            y = 0.75 * max(.x$model[[1]]),
+            label = .x %>%
+              glance() %>%
+              select(r.squared)
+          )
+      }
+    )
+  )
+)
 
-glance(GCFR_pred_vs_obs_m)
-glance(GCFR_pred_vs_obs_m_exp)
-glance(SWAFR_pred_vs_obs_m)
-glance(SWAFR_pred_vs_obs_m_exp)
-
-visreg::visreg(GCFR_pred_vs_obs_m, gg = TRUE) +
-  geom_abline(intercept = 0, slope = 1, linetype = "dashed")
-visreg::visreg(SWAFR_pred_vs_obs_m, gg = TRUE) +
-  geom_abline(intercept = 0, slope = 1, linetype = "dashed")
-
-visreg::visreg(GCFR_pred_vs_obs_m_exp, gg = TRUE) +
-  geom_abline(intercept = 0, slope = 1, linetype = "dashed")
-visreg::visreg(SWAFR_pred_vs_obs_m_exp, gg = TRUE) +
-  geom_abline(intercept = 0, slope = 1, linetype = "dashed")
