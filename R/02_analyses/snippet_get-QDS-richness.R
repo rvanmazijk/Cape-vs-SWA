63b5b922daf373b70ce2b16fda74975b9cd92e20
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-09-01 17:14:29 +0200

    Add snippet for future use when BRT-ing at QDS-scale

--- R/02_analyses/snippet_get-QDS-richness.R
+++ R/02_analyses/snippet_get-QDS-richness.R
@@ -1,14 +1,13 @@
+# Get all QDS geocode names
+all_QDS <- levels(factor(GCFR_species$qdgc))
 # Initialise empty column for QDS richness
 GCFR_species@data$QDS_richness <- NULL
-for (i in seq_along(levels(factor(GCFR_species$qdgc)))) {
-  # Current QDS geocode name
-  current_QDS <- levels(factor(GCFR_species$qdgc))[[i]]
+for (QDS in all_QDS) {
   # Filter to only those QDS w/i the current HDS
   spp_in_qdgc <- GCFR_species@data %>%
     filter(qdgc == current_QDS) %>%
     select(species) %>%
     as_vector()
   # Store QDS richness in data
-  GCFR_species$QDS_richness[GCFR_species$qdgc == current_QDS] <-
-    length(spp_in_qdgc)
+  GCFR_species$QDS_richness[GCFR_species$qdgc == QDS] <- length(spp_in_qdgc)
 }
