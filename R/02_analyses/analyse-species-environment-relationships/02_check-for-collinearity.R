cd26e9a662abd0b039bdd89e274e087c559935bf
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-11-26 18:58:21 +0200

    Add collinearity-checking for QDS-scale too, run

--- R/02_analyses/analyse-species-environment-relationships/02_check-for-collinearity.R
+++ R/02_analyses/analyse-species-environment-relationships/02_check-for-collinearity.R
@@ -12,8 +12,20 @@ png(
   filename = glue("{output_path}/Cape_collinearity-check_0.8-cutoff_HDS.png"),
   width = 17, height = 15, units = "cm", res = 100
 )
-GCFR_collinearity <- removeCollinearity(
+GCFR_predictor_names_HDS <- removeCollinearity(
   raster.stack = GCFR_variables_HDS_stack[[-c(1, 2)]],  # exclude responses
+  select.variables = FALSE,  # To do manually
+  multicollinearity.cutoff = 0.8,  # My thumb-suck. TODO: try 0.7?
+  plot = TRUE
+)
+dev.off()
+
+png(
+  filename = glue("{output_path}/Cape_collinearity-check_0.8-cutoff_QDS.png"),
+  width = 17, height = 15, units = "cm", res = 100
+)
+GCFR_predictor_names_QDS <- removeCollinearity(
+  raster.stack = GCFR_data_QDS_stack[[-1]],  # exclude responses
   select.variables = FALSE,  # To do manually
   multicollinearity.cutoff = 0.8,  # My thumb-suck. TODO: try 0.7?
   plot = TRUE
@@ -26,8 +38,20 @@ png(
   filename = glue("{output_path}/SWA_collinearity-check_0.8-cutoff_HDS.png"),
   width = 17, height = 15, units = "cm", res = 100
 )
-SWAFR_collinearity <- removeCollinearity(
+SWAFR_predictor_names_HDS <- removeCollinearity(
   raster.stack = SWAFR_variables_HDS_stack[[-c(1, 2)]],
+  select.variables = FALSE,
+  multicollinearity.cutoff = 0.8,
+  plot = TRUE
+)
+dev.off()
+
+png(
+  filename = glue("{output_path}/SWA_collinearity-check_0.8-cutoff_QDS.png"),
+  width = 17, height = 15, units = "cm", res = 100
+)
+SWAFR_predictor_names_QDS <- removeCollinearity(
+  raster.stack = SWAFR_data_QDS_stack[[-1]],
   select.variables = FALSE,
   multicollinearity.cutoff = 0.8,
   plot = TRUE
@@ -37,8 +61,10 @@ dev.off()
 # Select final predictor variables ---------------------------------------------
 
 # Take first variable in each collinear cluster
-GCFR_predictor_names <- map_chr(GCFR_collinearity, 1)
-SWAFR_predictor_names <- map_chr(SWAFR_collinearity, 1)
+GCFR_predictor_names_QDS  %<>% map_chr(1)
+GCFR_predictor_names_HDS  %<>% map_chr(1)
+SWAFR_predictor_names_QDS %<>% map_chr(1)
+SWAFR_predictor_names_HDS %<>% map_chr(1)
 
 # For bare-minimum BRT work on UCT HPC:
 output_path <- here(
@@ -46,10 +72,19 @@ output_path <- here(
   "analyse-species-environment-relationships/run-on-UCT-HPC/"
 )
 write.csv(
-  GCFR_predictor_names,
-  glue("{output_path}/GCFR_predictor_names.csv")
+  GCFR_predictor_names_QDS,
+  glue("{output_path}/GCFR_predictor_names_QDS.csv")
 )
 write.csv(
-  SWAFR_predictor_names,
-  glue("{output_path}/SWAFR_predictor_names.csv")
+  GCFR_predictor_names_HDS,
+  glue("{output_path}/GCFR_predictor_names_HDS.csv")
 )
+write.csv(
+  SWAFR_predictor_names_QDS,
+  glue("{output_path}/SWAFR_predictor_names_QDS.csv")
+)
+write.csv(
+  SWAFR_predictor_names_QDS,
+  glue("{output_path}/SWAFR_predictor_names_HDS.csv")
+)
+
