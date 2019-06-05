ada1f52960aab4511a2b459ed3c2c20974653804
Ruan van Mazijk <ruanvmazijk@gmail.com>
2019-06-05 13:21:04 +0200

    Move roughness function to own script

--- analyses-May-2019/03_collate-and-transform-data.R
+++ analyses-May-2019/03_collate-and-transform-data.R
@@ -34,30 +34,7 @@ SWAFR_variables_masked2 <- map(var_names,
 
 # Define roughness **here** as the mean absolute difference of a focal cell's
 # value (in a 3 x 3 neighbourhood) to it's ca. 8 neighbours
-roughness <- function(x, ...) {
-  focal(
-    x = x,
-    w = matrix(1, nrow = 3, ncol = 3),
-    function(x, ...) {
-      focal_cell <- x[[5]]
-      if (is.na(focal_cell) | is.nan(focal_cell)) {
-        return(NA)
-      } else {
-        x <- x[!is.na(x) & !is.nan(x)]
-        diffs <- vector(length = length(x))
-        for (i in seq_along(diffs)) {
-          diffs[[i]] <-
-            if (!is.na(x[[i]]) & !is.nan(x[[i]])) {
-              abs(focal_cell - x[[i]])
-            } else if (x[[i]] == focal_cell) {
-              NA
-            }
-        }
-        mean(diffs, na.rm = TRUE)
-      }
-    }
-  )
-}
+body(roughness)
 
 # Test
 foo <- roughness(GCFR_variables_masked2$pH)
