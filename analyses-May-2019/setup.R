70146042c0605fe5774c7f09c786142994183ddb
Ruan van Mazijk <ruanvmazijk@gmail.com>
2019-06-11 21:38:01 +0200

    Save work from today

--- analyses-May-2019/setup.R
+++ analyses-May-2019/setup.R
@@ -29,8 +29,12 @@ library(broom)  # to tidy model outputs
 library(vegan)
 
 # Figures
-library(rasterVis)  # fpr maps
+library(rasterVis)  # for maps
 library(ggfortify)  # for PCAs
+library(grid)
+# Panelling figures
+library(cowplot)
+library(patchwork)
 
 # Source helper functions ------------------------------------------------------
 
@@ -39,3 +43,55 @@ function_filenames <- list.files(
   full.names = TRUE
 )
 walk(function_filenames, source)
+
+# Global ggplot2-theme settings ------------------------------------------------
+
+my_theme <-
+  theme_bw() +
+  theme(
+    strip.background = element_blank(),
+    panel.grid = element_blank()
+  )
+theme_set(my_theme)
+
+# Global graphics objects ------------------------------------------------------
+
+my_palette <- c(
+  "#E69F00",  # Cape (GCFR) orange
+  "#307aa5"   # SWA (SWAFR) blue
+)
+var_colours <- c(
+  # <https://colourco.de/>
+  "grey50",   # grey   for elevation
+  "#507CC5",  # blue   for climate
+  "#37A541",  # greeen for NDVI
+  "#BA793E",  # brown  for soils
+  "red"       #        for PC1
+)
+var_shapes <- c(
+  17,  # triangle      for elevation
+  16,  # filled circle for MAP
+  1,   # open circle   for PDQ
+  15,  # square        for surfact T
+  4,   # x             for NDVI,
+  17,  # triangle      for CEC
+  16,  # filled circle for clay
+  1,   # open circle   for soil C
+  15   # square        for pH
+)
+
+var_names <- c(
+  # Environmental variable names in nice order
+  "Elevation",
+  "MAP",
+  "PDQ",
+  "Surface T",
+  "NDVI",
+  "CEC",
+  "Clay",
+  "Soil C",
+  "pH"
+)
+
+white_rect <- grid.rect(gp = gpar(col = "white"))
+# Useful when arranging panels
