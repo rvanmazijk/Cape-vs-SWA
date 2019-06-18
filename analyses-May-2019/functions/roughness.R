7034a036cde1b0d2050e9c970b285da981ecee54
Ruan van Mazijk <ruanvmazijk@gmail.com>
2019-06-18 12:24:35 +0200

    Save

--- analyses-May-2019/functions/roughness.R
+++ analyses-May-2019/functions/roughness.R
@@ -1,17 +1,16 @@
 # Define roughness **here** as the mean absolute difference of a focal cell's
 # value (in a 3 x 3 neighbourhood) to it's ca. 8 neighbours
 roughness <- function(x) {
-  focal(x, matrix(1, nrow = 3, ncol = 3), function(x) {
-    focal_cell <- x[5]
-    neighbour_cells <- x[
-      !is.na(x) &
-      !is.nan(x) &
-      x != focal_cell
-    ]
-    ifelse(!is.na(focal_cell) & !is.nan(focal_cell),
-      mean(abs(focal_cell - neighbour_cells)),
-      NA
-    )
+  raster::focal(x, matrix(1, nrow = 3, ncol = 3), function(x) {
+   focal_cell <- x[5]
+   focal_exists <- (!is.na(focal_cell)) & (!is.nan(focal_cell))
+   if (focal_exists) {
+      neighbour_exists <- (!is.na(x)) & (!is.nan(x)) & (x != focal_cell)
+      neighbour_cells <- x[neighbour_exists]
+      return(mean(abs(focal_cell - neighbour_cells)))
+    } else {
+      return(NA)
+    }
   })
 }
 
@@ -24,3 +23,7 @@ roughness_cells <- function(x) {
   }
   mean(out)
 }
+# Or
+#roughness_cells <- function(x) {
+#  mean(imap_dbl(x, ~mean(abs(.x - x[-.y]))))
+#}
