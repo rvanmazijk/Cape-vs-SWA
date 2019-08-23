9d6b36f39d907f6fa2af0dcd4296908d318f2cd3
Ruan van Mazijk <ruanvmazijk@gmail.com>
2019-08-23 13:41:38 +0200

    Remove trailing '/' from paths, makes OGR import more reliable across platforms

--- draft-02/R/functions/helper-functions.R
+++ draft-02/R/functions/helper-functions.R
@@ -15,10 +15,10 @@ import_region_polygons <- function(borders_dir =
 
   # GCFR -----------------------------------------------------------------------
 
-  GCFR_border          <- readOGR(glue("{borders_dir}/GCFR_border/"))
-  GCFR_border_buffered <- readOGR(glue("{borders_dir}/GCFR_border_buffered/"))
-  GCFR_box             <- readOGR(glue("{borders_dir}/GCFR_box/"))
-  GCFR_QDS             <- readOGR(glue("{borders_dir}/GCFR_QDS/"))
+  GCFR_border          <- readOGR(glue("{borders_dir}/GCFR_border"))
+  GCFR_border_buffered <- readOGR(glue("{borders_dir}/GCFR_border_buffered"))
+  GCFR_box             <- readOGR(glue("{borders_dir}/GCFR_box"))
+  GCFR_QDS             <- readOGR(glue("{borders_dir}/GCFR_QDS"))
   assign_global(GCFR_border)
   assign_global(GCFR_border_buffered)
   assign_global(GCFR_box)
@@ -26,10 +26,10 @@ import_region_polygons <- function(borders_dir =
 
   # SWAFR ----------------------------------------------------------------------
 
-  SWAFR_border          <- readOGR(glue("{borders_dir}/SWBP_Mike-Cramer/"))
-  SWAFR_border_buffered <- readOGR(glue("{borders_dir}/SWAFR_border_buffered/"))
-  SWAFR_box             <- readOGR(glue("{borders_dir}/SWAFR_box/"))
-  SWAFR_QDS             <- readOGR(glue("{borders_dir}/SWAFR_QDS/"))
+  SWAFR_border          <- readOGR(glue("{borders_dir}/SWBP_Mike-Cramer"))
+  SWAFR_border_buffered <- readOGR(glue("{borders_dir}/SWAFR_border_buffered"))
+  SWAFR_box             <- readOGR(glue("{borders_dir}/SWAFR_box"))
+  SWAFR_QDS             <- readOGR(glue("{borders_dir}/SWAFR_QDS"))
   assign_global(SWAFR_border)
   assign_global(SWAFR_border_buffered)
   assign_global(SWAFR_box)
