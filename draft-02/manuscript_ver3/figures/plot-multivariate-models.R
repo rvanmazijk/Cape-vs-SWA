95a44342ebf06f9d7304dea252cd12cd6aeaba82
Ruan van Mazijk <ruanvmazijk@gmail.com>
2019-09-13 16:35:00 +0200

    Move figure code from analyses.Rmd -> R-scripts

---
+++ draft-02/manuscript_ver3/figures/plot-multivariate-models.R
@@ -0,0 +1,72 @@
+# Plot multivariate models
+
+models_R2adjs <- models_summary %>%
+  group_by(response) %>%
+  summarise(adj.r.squared = adj.r.squared %>%
+    unique() %>%
+    round(digits = 2)
+  )
+
+models_summary_for_plot <- models_summary %>%
+  mutate(
+    response = case_when(
+      response == "QDS_richness" ~ "(a)~QDS~~(italic(R)[adj]^2=='0.20')",
+      response == "HDS_richness" ~ "(b)~HDS~~(italic(R)[adj]^2=='0.21')",
+      response == "DS_richness"  ~ "(c)~DS~~(italic(R)[adj]^2=='0.78')"
+    ),
+    region =
+      case_when(
+        str_detect(term, "regionSWAFR") ~ "SWAFR",
+        str_detect(term, "regionGCFR")  ~ "GCFR",
+        TRUE                            ~ "Main effect only"
+      ) %>%
+      factor(levels = c("Main effect only", "GCFR", "SWAFR")),
+    term = term %>%
+      str_replace_all("_", " ") %>%
+      str_remove_all("regionSWAFR:") %>%
+      str_remove_all("regionGCFR:") %>%
+      str_replace_all("regionSWAFR", "SWAFR") %>%
+      factor(levels = c(var_names, "SWAFR")),
+    sig = ifelse(p.value < 0.05, "< 0.05", "NS")
+  )
+
+ggplot(models_summary_for_plot) +
+  aes(
+    term, estimate,
+    fill = region, group = region, shape = region,
+    alpha = sig
+  ) +
+  geom_hline(yintercept = 0, linetype = "dashed", colour = "grey75") +
+  geom_errorbar(
+    aes(ymin = conf.low, ymax = conf.high),
+    position = position_dodge(width = 0.25),
+    width = 0
+  ) +
+  geom_point(position = position_dodge(width = 0.25), size = 2) +
+  labs(
+    x = "Heterogeneity predictor",
+    y = bquote(
+      "Effect"~~
+        "(log"["10"]*italic("S")*")"
+    )
+  ) +
+  scale_fill_manual(values = c(NA, "black", "white")) +
+  scale_shape_manual(values = c(4, 21, 21)) +
+  scale_alpha_manual(values = c(1, 0.25)) +
+  facet_wrap(~response, nrow = 3, scales = "free_y", labeller = label_parsed) +
+  guides(
+    fill = FALSE,
+    shape = guide_legend(
+      title = "Effect type",
+      override.aes = list(fill = c(NA, "black", "white"))
+    ),
+    alpha = guide_legend(
+      title = bquote(italic("P")["Effect"]),
+      override.aes = list(alpha = c(1, 0.25), linetype = NA)
+    )
+  ) +
+  theme(
+    axis.text.x  = element_text(angle = 90, hjust = 1, vjust = 0.5),
+    axis.text.y  = element_text(angle = 90, hjust = 0.5),
+    strip.text.x = element_text(angle =  0, hjust = 0)
+  )
---
+++ draft-02/manuscript_ver3/figures/plot-richness-distributions-partitions.R
@@ -0,0 +1,120 @@
+# Plot richness distributions & partitions
+
+data_for_plot <- data %$%
+  rbind(
+    QDS %>%
+      dplyr::select(region, QDS_richness) %>%
+      rename(richness = QDS_richness) %>%
+      add_column(scale = "QDS", turnover_prop = NA),
+    HDS %>%
+      dplyr::select(region, HDS_richness, QDS_turnover_prop) %>%
+      rename(richness = HDS_richness, turnover_prop = QDS_turnover_prop) %>%
+      add_column(scale = "HDS"),
+    DS %>%
+      dplyr::select(region, DS_richness, HDS_turnover_prop) %>%
+      rename(richness = DS_richness, turnover_prop = HDS_turnover_prop) %>%
+      add_column(scale = "DS")
+  ) %>%
+  as_tibble() %>%
+  gather(metric, metric_value, richness, turnover_prop) %>%
+  unite(metric_scale, scale, metric) %>%
+  na.exclude()
+
+x_axis_labels <- list(
+  QDS_richness      = bquote(italic("S")["QDS"]),
+  HDS_richness      = bquote(italic("S")["HDS"]),
+  HDS_turnover_prop = bquote(italic("T")["QDS"]/bar(italic("S"))["QDS"]),
+  DS_richness       = bquote(italic("S")["DS"]),
+  DS_turnover_prop  = bquote(italic("T")["HDS"]/bar(italic("S"))["HDS"])
+)
+hist_plots <- map(unique(data_for_plot$metric_scale),
+  ~ data_for_plot %>%
+    filter(metric_scale == .x) %>%
+    ggplot(aes(metric_value, fill = region)) +
+    geom_histogram(
+      bins = case_when(
+        str_detect(.x, "QDS") ~ 30,
+        str_detect(.x, "HDS") ~ 20,
+        str_detect(.x, "DS")  ~ 10
+      ),
+      position = "dodge",
+      colour = "black"
+    ) +
+    scale_fill_manual(name = "Region", values = c("black", "white")) +
+    labs(
+      x = x_axis_labels[[.x]],
+      y = case_when(
+        str_detect(.x, "QDS") ~ "No. QDS",
+        str_detect(.x, "HDS") ~ "No. HDS",
+        str_detect(.x, "DS")  ~ "No. DS"
+      )
+    ) +
+    theme(
+      legend.position = c(0.8, 0.8),
+      axis.text.y     = element_text(angle = 90, hjust = 0.5)
+    )
+)
+names(hist_plots) <- unique(data_for_plot$metric_scale)
+
+plot_lim <- data %$%
+  HDS %$%
+  ceiling(max(c(mean_QDS_richness, QDS_turnover))) + 10
+S_HDS_background <- plot_lim %>%
+  {seq(from = 0, to = ., by = 10)} %>%
+  {expand.grid(x = ., y = .)} %>%
+  mutate(z = x + y)
+S_HDS_background_plot <- ggplot(S_HDS_background) +
+  lims(x = c(0, plot_lim), y = c(0, plot_lim)) +
+  geom_contour(
+    mapping     = aes(x, y, z = z),
+    binwidth    = 500,
+    colour      = "grey90",
+    show.legend = TRUE
+  ) +
+  geom_abline(
+    intercept = 0, slope = 1,
+    linetype = "dashed", colour = "grey25"
+  ) +
+  labs(
+    x = bquote(italic("T")["QDS"]),
+    y = bquote(bar(italic("S"))["QDS"])
+  ) +
+  theme(axis.text.y = element_text(angle = 90, hjust = 0.5))
+
+partition_plot <- S_HDS_background_plot +
+  geom_point(
+    data    = data$HDS,
+    mapping = aes(QDS_turnover, mean_QDS_richness, fill = region),
+    shape   = 21
+  ) +
+  scale_fill_manual(values = c("black", "white")) +
+  theme(legend.position = "none")
+
+# Add labels to S_HDS contours manually
+label1500 <- bquote(italic("S")["HDS"] == 1500)
+partition_plot <- partition_plot +
+  geom_text(
+    data = tibble(
+      add_turnover      = c( 10,   10,        60,  560, 1060),
+      mean_QDS_richness = c(500, 1000,      1450, 1500, 1500),
+      HDS_richness      = c(500, 1000, label1500, 2000, 2500)
+    ),
+    mapping = aes(add_turnover, mean_QDS_richness, label = HDS_richness),
+    angle = -45, vjust = -0.5, colour = "grey50", size = 2.5,
+    parse = TRUE
+  ) +
+  # Flip partition plot to get axes to line up across panels (b/o text heights)
+  coord_flip()
+
+hist_plots[c("QDS_richness", "HDS_turnover_prop")] %<>% map(
+  ~.x + theme(legend.position = "none")
+)
+# Plot panels
+hist_plots %$% plot_grid(
+  QDS_richness,   HDS_richness,
+  partition_plot, HDS_turnover_prop,
+  nrow = 2,
+  labels = glue("({letters[1:4]})"),
+  label_x = 0.125, label_y = 0.975
+)
+# TODO: Plot DS-scale versions for SI
