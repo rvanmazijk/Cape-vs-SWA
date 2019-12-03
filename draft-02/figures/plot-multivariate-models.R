71bc2881ae2f347cd07d210c3d4716d2c5306230
Ruan van Mazijk <ruanvmazijk@gmail.com>
2019-12-03 16:49:55 +0200

    Update figure 4

--- draft-02/figures/plot-multivariate-models.R
+++ draft-02/figures/plot-multivariate-models.R
@@ -11,9 +11,9 @@ models_R2adjs
 models_summary_for_plot <- models_summary %>%
   mutate(
     response = case_when(
-      response == "QDS_richness" ~ "(a)~~QDS~(italic(R)[adj]^2=='0.24')",
-      response == "HDS_richness" ~ "(b)~~HDS~(italic(R)[adj]^2=='0.33')",
-      response == "DS_richness"  ~ "(c)~~DS~(italic(R)[adj]^2=='0.61')"
+      response == "QDS_richness" ~ "(a)~~QDS~(italic(R)[adj.]^2=='0.24')",
+      response == "HDS_richness" ~ "(b)~~HDS~(italic(R)[adj.]^2=='0.33')",
+      response == "DS_richness"  ~ "(c)~~DS~(italic(R)[adj.]^2=='0.61')"
     ),
     region =
       case_when(
@@ -22,17 +22,21 @@ models_summary_for_plot <- models_summary %>%
       ) %>%
       factor(levels = c("Main effect", "SWAFR")),
     term = term %>%
-      str_replace_all("_", " ") %>%
+      str_replace_all("_", "~") %>%
       str_remove_all(":regionSWAFR") %>%
       str_replace_all("regionSWAFR", "SWAFR") %>%
-      factor(levels = c(rev(var_names), "SWAFR")),
+      str_replace("^SWAFR$", "Region[SWAFR]") %>%
+      factor(levels = c(rev(str_replace(var_names, " ", "~")), "Region[SWAFR]")),
     sig = ifelse(p.value < 0.05, "< 0.05", "NS")
   ) %>%
   group_by(response, term) %>%
   mutate(term_type = case_when(
-    (n() == 2) & (region == "SWAFR")                     ~ "SWAFR vs GCFR",
-    (n() == 2) & (region == "Main effect")               ~ "GCFR",
-    (n() == 1) & (region %in% c("Main effect", "SWAFR")) ~ "Main effect only"
+    #(n() == 2) & (region == "SWAFR")                     ~ "SWAFR vs GCFR",
+    #(n() == 2) & (region == "Main effect")               ~ "GCFR",
+    #(n() == 1) & (region %in% c("Main effect", "SWAFR")) ~ "Main effect only"
+                 (region == "SWAFR")       ~ "SWAFR vs GCFR",
+    (n() == 2) & (region == "Main effect") ~ "GCFR",
+    (n() == 1) & (region == "Main effect") ~ "Main effect only"
   ))
 models_summary_for_plot$term_type %<>% factor(levels = c(
   "Main effect only",
@@ -56,6 +60,7 @@ model_summary_plot <- ggplot(models_summary_for_plot) +
     x = bquote("Partial effect"~~"("*italic("S")*")"),
     y = "Predictor"
   ) +
+  scale_y_discrete(labels = parse(text = levels(models_summary_for_plot$term))) +
   scale_fill_manual(values = c(NA, "black", "white")) +
   scale_shape_manual(values = c(4, 21, 21)) +
   scale_alpha_manual(values = c(1, 0.25)) +
@@ -76,12 +81,15 @@ model_summary_plot <- ggplot(models_summary_for_plot) +
       override.aes = list(alpha = c(1, 0.25), linetype = NA)
     )
   ) +
-  theme(strip.text.x = element_text(angle =  0, hjust = 0))
+  theme(
+    strip.text.x = element_text(angle =  0, hjust = 0),
+    axis.text.x  = element_text(angle = 90, hjust = 1, vjust = 0.5)
+  )
 
 # Save to disc
 ggsave(
   here(
-    "draft-02/manuscript_ver3-4/figures",
+    "draft-02/figures",
     "plot-multivariate-models.pdf"
   ),
   model_summary_plot,
@@ -89,7 +97,7 @@ ggsave(
 )
 ggsave(
   here(
-    "draft-02/manuscript_ver3-4/figures",
+    "draft-02/figures",
     "plot-multivariate-models.png"
   ),
   model_summary_plot, dpi = 600,
