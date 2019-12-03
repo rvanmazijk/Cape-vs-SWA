85db6ee24f751aca95971e8cf516f989b266e68f
Ruan van Mazijk <ruanvmazijk@gmail.com>
2019-12-03 13:06:49 +0200

    Work on collinearity checks

--- draft-02/R/10_check-univariate-linearity.R
+++ draft-02/R/10_check-univariate-linearity.R
@@ -1,3 +1,5 @@
+# Import data ------------------------------------------------------------------
+
 GCFR_heterogeneity <- map(c(QDS = "QDS",
                              HDS = "HDS",
                              DS  = "DS"), function(each_scale) {
@@ -25,6 +27,8 @@ SWAFR_heterogeneity <- map(c(QDS = "QDS",
   vars
 })
 
+# Check for collinearity within regions' data ----------------------------------
+
 pdf("GCFR-QDS-log10-heterogeneity-pairs.pdf", width = 10, height = 10)
 pairs(log10(GCFR_heterogeneity$QDS),  cor = TRUE)
 dev.off()
@@ -49,23 +53,42 @@ pdf("SWAFR-DS-log10-heterogeneity-pairs.pdf", width = 10, height = 10)
 pairs(log10(SWAFR_heterogeneity$DS), cor = TRUE)
 dev.off()
 
+# Check for collinearity with both regions merged ------------------------------
+
+# .... Merge regions' data -----------------------------------------------------
+
+# Have different origins
+map(GCFR_heterogeneity,  origin)
+map(SWAFR_heterogeneity, origin)
+
+# Ammend origins to be same
 set_origin <- function(r, ox = 0, oy = 0) {
   origin(r) <- c(ox, oy)
   r
 }
-
-map(GCFR_heterogeneity,  origin)
-map(SWAFR_heterogeneity, origin)
-
 GCFR_heterogeneity2  <- map(GCFR_heterogeneity,  set_origin)
 SWAFR_heterogeneity2 <- map(SWAFR_heterogeneity, set_origin)
 
+# Check
 map(GCFR_heterogeneity2, origin)
 map(SWAFR_heterogeneity2, origin)
 
+# Merge
 heterogeneity2 <- map2(GCFR_heterogeneity2, SWAFR_heterogeneity2, merge)
+
+# Rename layers
 heterogeneity2 %<>% map(function(r) {
   names(r) <- str_replace_all(var_names, " ", "_")
   r
 })
-map(heterogeneity2, ~pairs(log10(.), cor = TRUE))
+
+# .... Do the collinearity check proper ----------------------------------------
+
+imap(heterogeneity2, function(each_scale, each_scales_name) {
+  pdf(
+    glue("{each_scales_name}-log10-heterogeneity-pairs.pdf"),
+    width = 10, height = 10
+  )
+  pairs(log10(each_scale), cor = TRUE)
+  dev.off()
+})
