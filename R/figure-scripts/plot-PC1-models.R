1b3c950b3ea17223099975505758f1048f961356
Ruan van Mazijk <ruanvmazijk@gmail.com>
2019-12-10 11:49:50 +0200

    Display outliers in PC1 model plots

--- R/figure-scripts/plot-PC1-models.R
+++ R/figure-scripts/plot-PC1-models.R
@@ -1,8 +1,17 @@
+# Import data ------------------------------------------------------------------
+
 data <- list(
-  QDS = read_csv(glue("{data_dir}/data-QDS.csv")),
-  HDS = read_csv(glue("{data_dir}/data-HDS.csv")),
-  DS  = read_csv(glue("{data_dir}/data-DS.csv"))
+  QDS = read_csv(glue("{data_dir}/data-QDS-w-residuals.csv")),
+  HDS = read_csv(glue("{data_dir}/data-HDS-w-residuals.csv")),
+  DS  = read_csv(glue("{data_dir}/data-DS-w-residuals.csv"))
 )
+
+# Identify outliers ------------------------------------------------------------
+
+data %<>% map(~ mutate(.x,
+  is_PC1_outlier = as_vector(scale(PC1_residual)          > 2),
+  is_MV_outlier  = as_vector(scale(multivariate_residual) > 2)
+))
 
 #map(heterogeneity_PCAs, summary)[2:4]
 #>          Proportion of Variance (PC1)
@@ -23,15 +32,16 @@ glance(m_DS)$r.squared
 master_ylim <- c(0, max(data$DS$DS_richness))
 
 QDS_plot <- ggplot(data$QDS, aes(PC1, QDS_richness, fill = region)) +
-  geom_point(shape = 21, size = 1.25) +
+  geom_point(aes(shape = is_PC1_outlier), size = 1.25) +
   geom_smooth(aes(colour = region), method = lm, se = FALSE) +
   labs(
     x     = "PC1 (42.44%)",
     y     = bquote(italic("S")),
     title = bquote("(a)  QDS ("*italic("R")^{"2"}*" = 0.14)")
   ) +
-  scale_fill_manual(name = "Region", values = c("black", "white")) +
-  scale_colour_manual(name = "Region", values = c("black", "grey50")) +
+  scale_fill_manual(values = c("black", "white")) +
+  scale_colour_manual(values = c("black", "grey50")) +
+  scale_shape_manual(values = c(21, 24)) +
   coord_cartesian(ylim = master_ylim) +
   theme(
     legend.position = "none",
@@ -39,13 +49,14 @@ QDS_plot <- ggplot(data$QDS, aes(PC1, QDS_richness, fill = region)) +
   )
 
 HDS_plot <- ggplot(data$HDS, aes(PC1, HDS_richness)) +
-  geom_point(aes(fill = region), shape = 21, size = 1.5) +
+  geom_point(aes(fill = region, shape = is_PC1_outlier), size = 1.5) +
   geom_smooth(method = lm, colour = "black", se = FALSE) +
   labs(
     x     = "PC1 (39.02%)",
     title = bquote("(b)  HDS ("*italic("R")^{"2"}*" = 0.19)")
   ) +
-  scale_fill_manual(name = "Region", values = c("black", "white")) +
+  scale_fill_manual(values = c("black", "white")) +
+  scale_shape_manual(values = c(21, 24)) +
   coord_cartesian(ylim = master_ylim) +
   theme(
     legend.position = "none",
@@ -55,14 +66,25 @@ HDS_plot <- ggplot(data$HDS, aes(PC1, HDS_richness)) +
   )
 
 DS_plot <- ggplot(data$DS, aes(PC1, DS_richness)) +
-  geom_point(aes(fill = region), shape = 21, size = 2.0) +
+  geom_point(aes(fill = region, shape = is_PC1_outlier), size = 2.0) +
   geom_smooth(method = lm, colour = "black", se = FALSE) +
   labs(
     x      = "PC1 (41.26%)",
     title  = bquote("(c)  DS ("*italic("R")^{"2"}*" = 0.28)")
   ) +
-  scale_fill_manual(name = "Region", values = c("black", "white")) +
+  scale_fill_manual(values = c("black", "white")) +
+  scale_shape_manual(values = c(21, 24), labels = c("\nHotspots", "")) +
   coord_cartesian(ylim = master_ylim) +
+  guides(
+    fill = guide_legend(
+      title = "Region", order = 1,
+      override.aes = list(shape = c(21, 21), fill = c("black", "white"))
+    ),
+    shape = guide_legend(
+      title = NULL, order = 2,
+      override.aes = list(shape = c(24, 24), fill = c("black", "white"))
+    )
+  ) +
   theme(
     axis.ticks.y = element_blank(),
     axis.text.y  = element_blank(),
@@ -79,18 +101,12 @@ all_scales_plots <- plot_grid(
 
 # Save to disc
 ggsave(
-  here(
-    "draft-02/manuscript_ver3/figures",
-    "plot-PC1-models.pdf"
-  ),
+  here("figures/plot-PC1-models.pdf"),
   all_scales_plots,
   width = 9, height = 3
 )
 ggsave(
-  here(
-    "draft-02/manuscript_ver3/figures",
-    "plot-PC1-models.pNG"
-  ),
+  here("figures/plot-PC1-models.png"),
   all_scales_plots, dpi = 600,
   width = 9, height = 3
 )
