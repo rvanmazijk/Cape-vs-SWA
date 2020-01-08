46faa679ea73e41bc4daf0b1cce12f6e4aee0a47
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-01-08 10:39:35 +0200

    Generate sans outliers mv model plot and results table

--- R/analyses/08_re-explaining-richness-wo-outliers.R
+++ R/analyses/08_re-explaining-richness-wo-outliers.R
@@ -149,8 +149,8 @@ summary(m_DS)
 data2 <- data %>%
   map(filter, !is_MV_outlier)
 
+# Fit multivariate models
 predictor_names <- c(str_replace_all(var_names, " ", "_"), "PC1")
-
 full_formula <- predictor_names[predictor_names != "PC1"] %>%
   {c(., paste(., "* region"))} %>%
   paste(collapse = " + ")
@@ -163,58 +163,7 @@ m_QDS_richness %<>% step(direction = "backward", trace = 0)
 m_HDS_richness %<>% step(direction = "backward", trace = 0)
 m_DS_richness  %<>% step(direction = "backward", trace = 0)
 
-# Summarise models (pre-reparameterisation)
-models1 <- list(
-  QDS_richness = m_QDS_richness,
-  HDS_richness = m_HDS_richness,
-  DS_richness  = m_DS_richness
-)
-models_summary1 <- models1 %>%
-  map_df(.id = "response", tidy, conf.int = TRUE) %>%
-  dplyr::select(-std.error, -statistic) %>%
-  filter(term != "(Intercept)")
-models1_R2 <- models1 %>%
-  map_df(.id = "response", glance) %>%
-  dplyr::select(response, adj.r.squared)
-models_summary1 %<>% full_join(models1_R2)
-
-# Save results out (especially for Tony)
-models_summary1_95 <- models1 %>%
-  map_df(.id = "response", tidy, conf.int = TRUE, conf.level = 0.95) %>%
-  rename(conf.low.05 = conf.low, conf.high.05 = conf.high)
-models_summary1_99 <- models1 %>%
-  map_df(.id = "response", tidy, conf.int = TRUE, conf.level = 0.99) %>%
-  rename(conf.low.01 = conf.low, conf.high.01 = conf.high)
-full_join(models_summary1_95, models_summary1_99) %>%
-  dplyr::select(-std.error, -statistic) %>%
-  mutate_if(is.numeric, ~round(., digits = 3)) %>%
-  mutate(p.value = ifelse(p.value < 0.001, "< 0.001", p.value)) %>%
-  write_csv(here("alt-refit-model-summary-for-Tony.csv"))
-
-reparameterise <- function(m) {
-  response <- colnames(m$model)[[1]]
-  dataset <- data2 %$% {
-    if      (response == "QDS_richness") QDS
-    else if (response == "HDS_richness") HDS
-    else if (response == "DS_richness")  DS
-  }
-  preds_w_interactions <- m %$%
-    coefficients %>%
-    names() %>%
-    magrittr::extract(str_which(., ":regionSWAFR"))
-  reparameterisation <- preds_w_interactions %>%
-    str_remove(":regionSWAFR") %>%
-    {glue("-{.}")} %>%
-    paste(collapse = " ")
-  update(m,
-    formula = glue(". ~ . {reparameterisation}"),
-    data    = dataset
-  )
-}
-m_QDS_richness %<>% reparameterise()
-m_HDS_richness %<>% reparameterise()
-m_DS_richness  %<>% reparameterise()
-
+# Check
 par(mfrow = c(2, 2))
 plot(m_QDS_richness)
 plot(m_HDS_richness)
@@ -247,7 +196,7 @@ full_join(models_summary_95, models_summary_99) %>%
   dplyr::select(-std.error, -statistic) %>%
   mutate_if(is.numeric, ~round(., digits = 3)) %>%
   mutate(p.value = ifelse(p.value < 0.001, "< 0.001", p.value)) %>%
-  write_csv(here("refit-model-summary-for-Tony.csv"))
+  write_csv(here("results/refit-model-summary-for-Tony.csv"))
 
 models_R2adjs <- models_summary %>%
   group_by(response) %>%
@@ -269,35 +218,49 @@ models_summary_for_plot <- models_summary %>%
     region =
       case_when(
         str_detect(term, "regionSWAFR") ~ "SWAFR",
-        str_detect(term, "regionGCFR")  ~ "GCFR",
-        TRUE                            ~ "Main effect only"
+        TRUE                            ~ "Main effect"
       ) %>%
-      factor(levels = c("Main effect only", "GCFR", "SWAFR")),
+      factor(levels = c("Main effect", "SWAFR")),
     term = term %>%
-      str_replace_all("_", " ") %>%
-      str_remove_all("regionSWAFR:") %>%
-      str_remove_all("regionGCFR:") %>%
+      str_replace_all("_", "~") %>%
+      str_remove_all(":regionSWAFR") %>%
       str_replace_all("regionSWAFR", "SWAFR") %>%
-      factor(levels = c(rev(var_names), "SWAFR")),
+      str_replace("^SWAFR$", "Region[SWAFR]") %>%
+      factor(levels = c(rev(str_replace(var_names, " ", "~")), "Region[SWAFR]")),
     sig = ifelse(p.value < 0.05, "< 0.05", "NS")
-  )
+  ) %>%
+  group_by(response, term) %>%
+  mutate(term_type = case_when(
+    #(n() == 2) & (region == "SWAFR")                     ~ "SWAFR vs GCFR",
+    #(n() == 2) & (region == "Main effect")               ~ "GCFR",
+    #(n() == 1) & (region %in% c("Main effect", "SWAFR")) ~ "Main effect only"
+    (region == "SWAFR")       ~ "SWAFR vs GCFR",
+    (n() == 2) & (region == "Main effect") ~ "GCFR",
+    (n() == 1) & (region == "Main effect") ~ "Main effect only"
+  ))
+models_summary_for_plot$term_type %<>% factor(levels = c(
+  "Main effect only",
+  "GCFR",
+  "SWAFR vs GCFR"
+))
 
 model_summary_plot <- ggplot(models_summary_for_plot) +
   aes(
     estimate, term, estimate,
-    fill = region, group = region, shape = region,
+    fill = term_type, group = term_type, shape = term_type,
     alpha = sig
   ) +
   geom_vline(xintercept = 0, linetype = "dashed", colour = "grey75") +
   geom_errorbarh(
     aes(xmin = conf.low, xmax = conf.high),
-    height = 0.1
+    height = 0
   ) +
   geom_point(size = 2) +
   labs(
-    x = bquote("Effect"~~"("*italic("S")*")"),
-    y = "Heterogeneity predictor"
+    x = bquote("Partial effect"~~"("*italic("S")*")"),
+    y = "Predictor"
   ) +
+  scale_y_discrete(labels = parse(text = levels(models_summary_for_plot$term))) +
   scale_fill_manual(values = c(NA, "black", "white")) +
   scale_shape_manual(values = c(4, 21, 21)) +
   scale_alpha_manual(values = c(1, 0.25)) +
@@ -310,7 +273,7 @@ model_summary_plot <- ggplot(models_summary_for_plot) +
   guides(
     fill = FALSE,
     shape = guide_legend(
-      title = "Effect type",
+      title = "Effect type", order = 1,
       override.aes = list(fill = c(NA, "black", "white"))
     ),
     alpha = guide_legend(
@@ -318,17 +281,22 @@ model_summary_plot <- ggplot(models_summary_for_plot) +
       override.aes = list(alpha = c(1, 0.25), linetype = NA)
     )
   ) +
-  theme(strip.text.x = element_text(angle =  0, hjust = 0))
+  theme(
+    strip.text.x = element_text(angle =  0, hjust = 0),
+    axis.text.x  = element_text(angle = 90, hjust = 1, vjust = 0.5)
+  )
 
 model_summary_plot
 
 # Save to disc
 ggsave(
-  here(
-    "draft-02/manuscript_ver3-4/figures",
-    "plot-refit-multivariate-models.pdf"
-  ),
+  here("figures/plot-refit-multivariate-models.pdf"),
   model_summary_plot,
+  width = 7, height = 4
+)
+ggsave(
+  here("figures/plot-refit-multivariate-models.png"),
+  model_summary_plot, dpi = 600,
   width = 7, height = 4
 )
 
