0650f59c07a7916ef9b7912cfb1f8c269c261e4a
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-02-11 12:44:15 +0200

    Tidying up and making final-ish version of this script(s)

--- for-Dryad/analyses/generate-richness-and-heterogeneity-data.R
+++ for-Dryad/analyses/generate-richness-and-heterogeneity-data.R
@@ -8,7 +8,8 @@ SWAFR_border_buffered <-
 GCFR_box  <- readOGR(here("data/derived-data/borders/GCFR_box"))
 SWAFR_box <- readOGR(here("data/derived-data/borders/SWAFR_box"))
 
-# Import Larsen grid polygons
+# Import Larsen grid polygons --------------------------------------------------
+
 ZA_EDS <- readOGR(here("data/raw-data/QDGC/qdgc_zaf"), layer = "qdgc_03_zaf")
 AU_EDS <- readOGR(here("data/raw-data/QDGC/qdgc_aus"), layer = "qdgc_03_aus")
 ZA_QDS <- readOGR(here("data/raw-data/QDGC/qdgc_zaf"), layer = "qdgc_02_zaf")
@@ -17,6 +18,9 @@ ZA_HDS <- readOGR(here("data/raw-data/QDGC/qdgc_zaf"), layer = "qdgc_01_zaf")
 AU_HDS <- readOGR(here("data/raw-data/QDGC/qdgc_aus"), layer = "qdgc_01_aus")
 
 # Generate polygons I need -----------------------------------------------------
+
+# Merge regions' borders
+borders_buffered <- rbind(GCFR_border_buffered, SWAFR_border_buffered)
 
 # Crop each Larsen grid to regions' extents
 GCFR_EDS  <- crop(ZA_EDS, GCFR_box)
@@ -30,9 +34,6 @@ SWAFR_HDS <- crop(AU_HDS, SWAFR_box)
 Larsen_grid_EDS <- rbind(GCFR_EDS, SWAFR_EDS)
 Larsen_grid_QDS <- rbind(GCFR_QDS, SWAFR_QDS)
 Larsen_grid_HDS <- rbind(GCFR_HDS, SWAFR_HDS)
-
-# Merge regions' borders
-borders_buffered <- rbind(GCFR_border_buffered, SWAFR_border_buffered)
 
 # Tidy up Larsen grid data and note QDS, HDS and DS
 Larsen_grid_EDS$edgc <- as.character(Larsen_grid_EDS$qdgc)
@@ -57,6 +58,7 @@ Larsen_grid_QDS@data[, c("lon", "lat", "areakm2")] %<>%
 Larsen_grid_HDS@data[, c("lon", "lat", "areakm2")] %<>%
   map(as.character) %>%
   map(as.numeric)
+# NOTE: lon, lat data for grid polygons are the midpoints
 
 # Detemine which DS, HDS & QDS have all 4 of their HDS, QDS & EDS --------------
 # (within the regions' borders)
@@ -69,12 +71,14 @@ Larsen_grid_EDS@data <- cbind(
 # For larger cells, just use longitude for region classification,
 # because later I filter the cells based on whether their constituent cells
 # are in the regions (from EDS above)
-Larsen_grid_QDS@data$region <- ifelse(Larsen_grid_QDS@data$lon > 90,
-  "SWAFR", "GCFR"
-)
-Larsen_grid_HDS@data$region <- ifelse(Larsen_grid_HDS@data$lon > 90,
-  "SWAFR", "GCFR"
-)
+Larsen_grid_QDS@data$region <-
+  ifelse(Larsen_grid_QDS@data$lon > 90,
+    "SWAFR", "GCFR"
+  )
+Larsen_grid_HDS@data$region <-
+  ifelse(Larsen_grid_HDS@data$lon > 90,
+    "SWAFR", "GCFR"
+  )
 
 # Filter to EDS that are within the regions' borders
 Larsen_grid_EDS <- Larsen_grid_EDS[!is.na(Larsen_grid_EDS$region), ]
@@ -113,32 +117,54 @@ DS_w_all_HDS <- Larsen_grid_HDS_data %>%
   filter(n_HDS == 4) %>%
   pull(dgc)
 
-# Plot to check
-plot(border = "green", Larsen_grid_EDS[
-  Larsen_grid_EDS$qdgc %in% QDS_w_all_EDS &
-  Larsen_grid_EDS$region == "GCFR",
-])
-plot(border = "red", add = TRUE, Larsen_grid_QDS[
-  Larsen_grid_QDS$hdgc %in% HDS_w_all_QDS &
-  Larsen_grid_QDS$region == "GCFR",
-])
-plot(border = "blue", add = TRUE, Larsen_grid_HDS[
-  Larsen_grid_HDS$dgc %in% DS_w_all_HDS &
-  Larsen_grid_HDS$region == "GCFR",
-])
+# Plot grids and cell midpoints to check
+# NOTE:
+#   Plots EDS that belong to a QDS with all 4 EDS in region,
+#   **not** the QDS themselves,
+#   etc. for other scales
+if (FALSE) {
+  # GCFR:
+  plot(border = "green", Larsen_grid_EDS[
+    Larsen_grid_EDS$qdgc %in% QDS_w_all_EDS &
+    Larsen_grid_EDS$region == "GCFR",
+  ])
+  points(col = "green",
+    Larsen_grid_EDS$lon[Larsen_grid_EDS$qdgc %in% QDS_w_all_EDS],
+    Larsen_grid_EDS$lat[Larsen_grid_EDS$qdgc %in% QDS_w_all_EDS]
+  )
+  plot(border = "red", add = TRUE, Larsen_grid_QDS[
+    Larsen_grid_QDS$hdgc %in% HDS_w_all_QDS &
+    Larsen_grid_QDS$region == "GCFR",
+  ])
+  points(col = "red",
+    Larsen_grid_QDS$lon[Larsen_grid_QDS$hdgc %in% HDS_w_all_QDS],
+    Larsen_grid_QDS$lat[Larsen_grid_QDS$hdgc %in% HDS_w_all_QDS]
+  )
+  plot(border = "blue", add = TRUE, Larsen_grid_HDS[
+    Larsen_grid_HDS$dgc %in% DS_w_all_HDS &
+    Larsen_grid_HDS$region == "GCFR",
+  ])
+  points(col = "blue",
+    Larsen_grid_HDS$lon[Larsen_grid_HDS$dgc %in% DS_w_all_HDS],
+    Larsen_grid_HDS$lat[Larsen_grid_HDS$dgc %in% DS_w_all_HDS]
+  )
 
-plot(border = "green", Larsen_grid_EDS[
-  Larsen_grid_EDS$qdgc %in% QDS_w_all_EDS &
-  Larsen_grid_EDS$region == "SWAFR",
-])
-plot(border = "red", add = TRUE, Larsen_grid_QDS[
-  Larsen_grid_QDS$hdgc %in% HDS_w_all_QDS &
-  Larsen_grid_QDS$region == "SWAFR",
-])
-plot(border = "blue", add = TRUE, Larsen_grid_HDS[
-  Larsen_grid_HDS$dgc %in% DS_w_all_HDS &
-  Larsen_grid_HDS$region == "SWAFR",
-])
+  # SWAFR:
+  plot(border = "green", Larsen_grid_EDS[
+    Larsen_grid_EDS$qdgc %in% QDS_w_all_EDS &
+    Larsen_grid_EDS$region == "SWAFR",
+  ])
+  plot(border = "red", add = TRUE, Larsen_grid_QDS[
+    Larsen_grid_QDS$hdgc %in% HDS_w_all_QDS &
+    Larsen_grid_QDS$region == "SWAFR",
+  ])
+  plot(border = "blue", add = TRUE, Larsen_grid_HDS[
+    Larsen_grid_HDS$dgc %in% DS_w_all_HDS &
+    Larsen_grid_HDS$region == "SWAFR",
+  ])
+}
+
+# Create my own blank rasters of/from the Larsen grids -------------------------
 
 # Collate richness data into grids ---------------------------------------------
 
