f26609e2ef1b93f50b5868e0245c4a73d523db89
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-02-18 13:42:08 +0200

    Cont. re-generating heterogeneity data

--- for-Dryad/analyses/03_generate-heterogeneity-data.R
+++ for-Dryad/analyses/03_generate-heterogeneity-data.R
@@ -148,7 +148,99 @@ heterogeneity_HDS_df$PC2 <- heterogeneity_HDS_PCA$x[, 2]
 heterogeneity_DS_df$PC1 <- heterogeneity_DS_PCA$x[, 1]
 heterogeneity_DS_df$PC2 <- heterogeneity_DS_PCA$x[, 2]
 
-# Merge richness data into dataframes ------------------------------------------
+# Save heterogeneity dataframes to disc ----------------------------------------
+
+write_csv(
+  heterogeneity_QDS_df,
+  here("for-Dryad/data/heterogeneity-data-QDS.csv")
+)
+
+write_csv(
+  heterogeneity_HDS_df,
+  here("for-Dryad/data/heterogeneity-data-HDS.csv")
+)
+
+write_csv(
+  heterogeneity_DS_df,
+  here("for-Dryad/data/heterogeneity-data-DS.csv")
+)
+
+# Rasterise heterogeneity dataframes -------------------------------------------
+
+heterogeneity_QDS_ras <- var_names %>%
+  str_replace_all(" ", "_") %>%
+  map(function(each_var) {
+    heterogeneity_QDS_df %>%
+      full_join(Larsen_grid_QDS_data) %>%
+      dplyr::select_("region", "qdgc", "lon", "lat", each_var) %>%
+      rasterise_data(each_var, Larsen_grid_QDS_ras)
+  }) %>%
+  stack() %>%
+  set_names(str_replace_all(var_names, " ", "_"))
+
+heterogeneity_HDS_ras <- var_names %>%
+  str_replace_all(" ", "_") %>%
+  map(function(each_var) {
+    heterogeneity_HDS_df %>%
+      full_join(Larsen_grid_HDS_data) %>%
+      dplyr::select_("region", "hdgc", "lon", "lat", each_var) %>%
+      rasterise_data(each_var, Larsen_grid_HDS_ras)
+  }) %>%
+  stack() %>%
+  set_names(str_replace_all(var_names, " ", "_"))
+
+heterogeneity_DS_ras <- var_names %>%
+  str_replace_all(" ", "_") %>%
+  map(function(each_var) {
+    heterogeneity_DS_df %>%
+      # Make DS-cell midpoints manually
+      mutate(
+        lon = dgc %>%
+          str_extract("E[0-9]{3}") %>%
+          str_remove("E") %>%
+          as.numeric() %>%
+          add(0.5),
+        lat = dgc %>%
+          str_extract("S[0-9]{2}") %>%
+          str_remove("S") %>%
+          as.numeric() %>%
+          add(0.5) %>%
+          multiply_by(-1)
+      ) %>%
+      dplyr::select_("region", "dgc", "lon", "lat", each_var) %>%
+      rasterise_data(each_var, Larsen_grid_DS_ras)
+  }) %>%
+  stack() %>%
+  set_names(str_replace_all(var_names, " ", "_"))
+
+# Plot to check
+if (FALSE) {
+  plot(heterogeneity_QDS_ras)
+  plot(heterogeneity_HDS_ras)
+  plot(heterogeneity_DS_ras)
+}
+
+# Save heterogeneity rasters to disc -------------------------------------------
+
+writeRaster(
+  heterogeneity_QDS_ras,
+  here("for-Dryad/data/raster-layers/heterogeneity-QDS"),
+  bylayer = TRUE, suffix = "names", format = "GTiff"
+)
+
+writeRaster(
+  heterogeneity_HDS_ras,
+  here("for-Dryad/data/raster-layers/heterogeneity-HDS"),
+  bylayer = TRUE, suffix = "names", format = "GTiff"
+)
+
+writeRaster(
+  heterogeneity_DS_ras,
+  here("for-Dryad/data/raster-layers/heterogeneity-DS"),
+  bylayer = TRUE, suffix = "names", format = "GTiff"
+)
+
+#####
 
 # Plot to check
 if (FALSE) {
