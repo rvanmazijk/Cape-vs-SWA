e15eb0c5547bdc8f586d3ba0f920dc14c0498395
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-02-19 13:31:27 +0200

    if (FALSE) {...}-ify PCA biplots for now

--- R/analyses/03_generate-heterogeneity-data.R
+++ R/analyses/03_generate-heterogeneity-data.R
@@ -291,60 +291,64 @@ writeRaster(
 
 #####
 
-# Plot PC-biplots
-PC_biplots <- pmap(list(heterogeneity_PCAs, heterogeneity, names(heterogeneity)),
-  ~ autoplot(..1, data = ..2, colour = "region",
-      loadings = TRUE,
-      loadings.colour = "black",
-      loadings.label = TRUE,
-      loadings.label.colour = "black",
-      loadings.label.hjust = -0.25,
-      loadings.label.size = 3
-    ) +
-    geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.5) +
-    geom_vline(xintercept = 0, linetype = "dashed", alpha = 0.5) +
-    lims(
-      x = case_when(
-        ..3 == "point1" ~ c(-0.10, 0.10),
-        ..3 == "QDS"    ~ c(-0.20, 0.20),
-        ..3 == "HDS"    ~ c(-0.25, 0.25),
-        ..3 == "DS"     ~ c(-0.25, 0.25)
-      ),
-      y = case_when(
-        ..3 == "point1" ~ c(-0.10, 0.10),
-        ..3 == "QDS"    ~ c(-0.20, 0.20),
-        ..3 == "HDS"    ~ c(-0.25, 0.25),
-        ..3 == "DS"     ~ c(-0.25, 0.25)
-      )
-    ) +
-    scale_colour_manual(name = "Region", values = c("grey25", "grey75")) +
-    theme(axis.text.y = element_text(angle = 90, hjust = 0.5))
-)
-my_legend <- get_legend(PC_biplots$point1)
-PC_biplots %<>% map(~ . + theme(legend.position = "none"))
-PC_biplots <- plot_grid(
-  plotlist = PC_biplots,
-  nrow = 2,
-  labels         = c("(a) 0.10°×0.10°", "(b) QDS", "(c) HDS", "(d) DS"),
-  label_fontface = "plain",
-  label_x        = 0.150,
-  label_y        = 0.975,
-  hjust          = 0
-)
-PC_biplots <- plot_grid(
-  PC_biplots, my_legend,
-  nrow = 1, rel_widths = c(1, 0.2)
-)
-PC_biplots
+if (FALSE) {
+  # Plot PC-biplots
+  PC_biplots <- pmap(list(heterogeneity_PCAs,
+                          heterogeneity,
+                          names(heterogeneity)),
+    ~ autoplot(..1, data = ..2, colour = "region",
+        loadings = TRUE,
+        loadings.colour = "black",
+        loadings.label = TRUE,
+        loadings.label.colour = "black",
+        loadings.label.hjust = -0.25,
+        loadings.label.size = 3
+      ) +
+      geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.5) +
+      geom_vline(xintercept = 0, linetype = "dashed", alpha = 0.5) +
+      lims(
+        x = case_when(
+          ..3 == "point1" ~ c(-0.10, 0.10),
+          ..3 == "QDS"    ~ c(-0.20, 0.20),
+          ..3 == "HDS"    ~ c(-0.25, 0.25),
+          ..3 == "DS"     ~ c(-0.25, 0.25)
+        ),
+        y = case_when(
+          ..3 == "point1" ~ c(-0.10, 0.10),
+          ..3 == "QDS"    ~ c(-0.20, 0.20),
+          ..3 == "HDS"    ~ c(-0.25, 0.25),
+          ..3 == "DS"     ~ c(-0.25, 0.25)
+        )
+      ) +
+      scale_colour_manual(name = "Region", values = c("grey25", "grey75")) +
+      theme(axis.text.y = element_text(angle = 90, hjust = 0.5))
+  )
+  my_legend <- get_legend(PC_biplots$point1)
+  PC_biplots %<>% map(~ . + theme(legend.position = "none"))
+  PC_biplots <- plot_grid(
+    plotlist = PC_biplots,
+    nrow = 2,
+    labels         = c("(a) 0.10°×0.10°", "(b) QDS", "(c) HDS", "(d) DS"),
+    label_fontface = "plain",
+    label_x        = 0.150,
+    label_y        = 0.975,
+    hjust          = 0
+  )
+  PC_biplots <- plot_grid(
+    PC_biplots, my_legend,
+    nrow = 1, rel_widths = c(1, 0.2)
+  )
+  PC_biplots
 
-# Save for SI
-ggsave(
-  here("figures/plot-PC-biplots.pdf"),
-  PC_biplots,
-  width = 8, height = 6
-)
-ggsave(
-  here("figures/plot-PC-biplots.png"),
-  PC_biplots,
-  width = 8, height = 6
-)
+  # Save for SI
+  ggsave(
+    here("figures/plot-PC-biplots.pdf"),
+    PC_biplots,
+    width = 8, height = 6
+  )
+  ggsave(
+    here("figures/plot-PC-biplots.png"),
+    PC_biplots,
+    width = 8, height = 6
+  )
+}
