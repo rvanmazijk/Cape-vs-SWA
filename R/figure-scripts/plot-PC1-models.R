af1ad3d7a3bb92208c4edeb880263fabd290d6e2
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-02-27 09:48:59 +0200

    Work on more general version of base::plot() implementation for PC1-model plots

--- R/figure-scripts/plot-PC1-models.R
+++ R/figure-scripts/plot-PC1-models.R
@@ -1,10 +1,172 @@
-# Import data ------------------------------------------------------------------
+plot_PC1_models <- function(dataset,
+                            keep_outliers = c(FALSE, TRUE),
+                            filename = "plot-PC1-models",
+                            ext = c("pdf", "png")) {
+
+
+
+  # Make changes to filename and data if needed --------------------------------
+
+
+  if (keep_outliers) {
+    # Pull out outlier points to plot as triangles
+    outliers <- map(dataset, filter, is_PC1_outlier)
+  } else {
+    # Otherwise just don't plot the outliers and make the filename clear
+    filename %<>% paste0("_refit")
+  }
+
+
+  # Initialise plotting environment on disc ------------------------------------
+
+  filename <- here("figures", glue("{filename}.{ext}"))
+  if (ext == "pdf") {
+    pdf(filename, width = 8, height = 3)
+  } else if (ext == "png") {
+    png(filename, width = 8, height = 3, units = "in", res = 600)
+  }
+
+  par(mfrow = c(1, 3))
+
+  # Define axis limits
+  my_ylim <- c(0, max(dataset$DS$DS_richness))
+  my_xlims <- map(dataset, ~range(.$PC1))
+
+  # Remove outliers from data used to plot dots (not triangles)
+  dataset %<>% map(filter, !is_PC1_outlier)
+
+  # Plot panel (a) (QDS) -------------------------------------------------------
+
+  par(mar = c(4, 4, 3, 0))
+
+  plot(
+    QDS_richness ~ PC1, dataset$QDS,
+    xlab = "PC1 (XX.XX%)",
+    xlim = my_xlims$QDS,
+    ylab = expression(paste(italic("S"))),
+    ylim = c(0, max(dataset$DS$DS_richness)),
+    pch  = 21, cex = 1.0,
+    bg   = ifelse(dataset$QDS$region == "GCFR", "black", "white")
+  )
+
+  if (keep_outliers) {
+    points(
+      outliers$QDS$PC1, outliers$QDS$QDS_richness,
+      pch = 24, cex = 1.0,
+      bg  = ifelse(outliers$QDS$region == "GCFR", "black", "white")
+    )
+  }
+
+  title(adj = 0, expression(paste(
+    "(a) QDS ("*italic("R")^2 == "0.XX)"
+  )))
+
+  #fit_GCFR <- predict(
+  #  m_QDS,
+  #  newdata = data.frame(region = "GCFR", PC1 = PC1_seq)
+  #)
+  #fit_SWAFR <- predict.lm(
+  #  m_QDS,
+  #  newdata = data.frame(region = "SWAFR", PC1 = PC1_seq)
+  #)
+  #lines(PC1_seq, fit_GCFR,  col = "black",  lwd = 3)
+  #lines(PC1_seq, fit_SWAFR, col = "grey50", lwd = 3)
+
+  legend(
+    x = -6, y = 3800,
+    legend  = unique(dataset$QDS$region),
+    pch     = 21,
+    pt.bg   = c("black", "white"),
+    box.col = NA
+  )
+
+  # Plot panel (b) (HDS) -------------------------------------------------------
+
+  par(mar = c(4, 2, 3, 2))
+
+  plot(
+    HDS_richness ~ PC1, dataset$HDS,
+    xlab = "PC1 (XX.XX%)",
+    xlim = my_xlims$HDS,
+    yaxt = "n",
+    ylab = "",
+    ylim = c(0, max(dataset$DS$DS_richness)),
+    pch  = 21, cex = 1.25,
+    bg   = ifelse(dataset$HDS$region == "GCFR", "black", "white")
+  )
+
+  if (keep_outliers) {
+    points(
+      outliers$HDS$PC1, outliers$HDS$HDS_richness,
+      pch = 24, cex = 1.25,
+      bg  = ifelse(outliers$HDS$region == "GCFR", "black", "white")
+    )
+  }
+
+  title(adj = 0, expression(paste(
+    "(b) HDS ("*italic("R")^2 == "0.XX)"
+  )))
+
+  #fit_GCFR <- predict(
+  #  m_HDS,
+  #  newdata = data.frame(region = "GCFR",  PC1 = PC1_seq)
+  #)
+  #fit_SWAFR <- predict.lm(
+  #  m_HDS,
+  #  newdata = data.frame(region = "SWAFR", PC1 = PC1_seq)
+  #)
+  #lines(PC1_seq, fit_GCFR,  col = "black",  lwd = 3)
+  #lines(PC1_seq, fit_SWAFR, col = "grey50", lwd = 3)
+
+  # Plot panel (c) (DS) --------------------------------------------------------
+
+  par(mar = c(4, 0, 3, 4))
+
+  plot(
+    DS_richness ~ PC1, dataset$DS,
+    xlab = "PC1 (XX.XX%)",
+    xlim = my_xlims$DS,
+    yaxt = "n",
+    ylab = "",
+    ylim = c(0, max(dataset$DS$DS_richness)),
+    pch  = 21, cex = 1.5,
+    bg   = ifelse(dataset$DS$region == "GCFR", "black", "white")
+  )
+
+  if (keep_outliers) {
+    points(
+      outliers$DS$PC1, outliers$HDS$DS_richness,
+      pch = 24, cex = 1.5,
+      bg  = ifelse(outliers$DS$region == "GCFR", "black", "white")
+    )
+  }
+
+  title(adj = 0, expression(paste(
+    "(c) DS ("*italic("R")^2 == "0.XX)"
+  )))
+
+  #fit <- predict.lm(m_DS, newdata = data.frame(PC1 = PC1_seq))
+  #lines(PC1_seq, fit,  col = "black",  lwd = 3)
+
+  # Close & reset plotting environment -----------------------------------------
+
+  dev.off()
+  par(op)
+}
 
 data <- list(
   QDS = read_csv(glue("{data_dir}/data-QDS-w-residuals.csv")),
   HDS = read_csv(glue("{data_dir}/data-HDS-w-residuals.csv")),
   DS  = read_csv(glue("{data_dir}/data-DS-w-residuals.csv"))
 )
+
+plot_PC1_models(data, keep_outliers = FALSE, ext = "pdf")
+plot_PC1_models(data, keep_outliers = FALSE, ext = "png")
+plot_PC1_models(data, keep_outliers = TRUE,  ext = "pdf")
+plot_PC1_models(data, keep_outliers = TRUE,  ext = "png")
+
+# Import data ------------------------------------------------------------------
+
 
 # Identify outliers ------------------------------------------------------------
 
