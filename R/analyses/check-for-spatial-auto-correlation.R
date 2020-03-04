a4d23464dc8d36bebcacbe1af24e2a262446be50
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-03-04 13:08:21 +0200

    Add more fiddling with SAC-code

--- R/analyses/check-for-spatial-auto-correlation.R
+++ R/analyses/check-for-spatial-auto-correlation.R
@@ -172,3 +172,89 @@ Moran_data %>%
     labs(x = "Spatial scale (º)", y = bquote("Moran's"~italic("I"))) +
     facet_wrap(~variable) +
     scale_colour_manual(name = "Region", values = c("black", "grey50"))
+
+mean_QDS_richness <- raster(glue(
+  "{data_dir}/raster-layers/",
+  "mean-QDS-richness_HDS.tif"
+))
+GCFR_mean_QDS_richness  <- crop(mean_QDS_richness, GCFR_border_buffered)
+SWAFR_mean_QDS_richness <- crop(mean_QDS_richness, SWAFR_border_buffered)
+
+mean_HDS_richness <- raster(glue(
+  "{data_dir}/raster-layers/",
+  "mean-HDS-richness_DS.tif"
+))
+GCFR_mean_HDS_richness  <- crop(mean_HDS_richness, GCFR_border_buffered)
+SWAFR_mean_HDS_richness <- crop(mean_HDS_richness, SWAFR_border_buffered)
+
+GCFR_HDS_turnover_prop <-
+  (GCFR_richness_HDS - GCFR_mean_QDS_richness)/
+   GCFR_richness_HDS
+SWAFR_HDS_turnover_prop <-
+  (SWAFR_richness_HDS - SWAFR_mean_QDS_richness)/
+   SWAFR_richness_HDS
+
+my_Moran(GCFR_HDS_turnover_prop)
+my_Moran(SWAFR_HDS_turnover_prop)
+
+plot(MoranLocal(GCFR_richness_QDS)[] ~ GCFR_richness_QDS[])
+plot(MoranLocal(GCFR_richness_HDS)[] ~ GCFR_HDS_turnover_prop[])
+plot(MoranLocal(GCFR_richness_DS)[] ~ GCFR_richness_DS[])
+
+MoranLocalP <- function(r, n_sim = 999) {
+  I <- MoranLocal(r)
+  null_I <- vector("list", length = n_sim)
+  for (i in 1:n_sim) {
+    r_copy <- r
+    r_copy[!is.na(r_copy[])] %<>% sample()
+    null_I[[i]] <- MoranLocal(r_copy)
+    cat(glue("{i}, "))
+  }
+  cat("\n\n")
+  null_I %<>% stack()
+  P <- I
+  P[] <- NA
+  for (i in 1:ncell(r)) {
+    if (!is.na(I[i])) {
+      P[i] <- rank(c(I[i], null_I[i]))[[1]]/(n_sim + 1)
+    }
+    cat(glue("{i}, "))
+  }
+  cat("\n\n")
+  # Make two-sided
+  P[!is.na(P[])] %<>% {ifelse(. > 0.5,
+    1 - . + (1/(n_sim + 1)),
+    .
+  )}
+  beepr::beep(8)
+  P
+}
+
+GCFR_richness_QDS_MoranLocalP <- MoranLocalP(GCFR_richness_QDS)
+plot(GCFR_richness_QDS_MoranLocalP)
+plot(GCFR_richness_QDS_MoranLocalP[] ~ GCFR_richness_QDS[])
+plot(MoranLocal(GCFR_richness_QDS))
+plot(MoranLocal(GCFR_richness_QDS) * (1 - 2*GCFR_richness_QDS_MoranLocalP))
+plot(
+  MoranLocal(GCFR_richness_QDS)[] * (1 - 2*GCFR_richness_QDS_MoranLocalP[]) ~
+  GCFR_richness_QDS[]
+)
+
+QDS_residuals <- raster(glue("{data_dir}/raster-layers/PC1_residual_QDS.tif"))
+GCFR_QDS_residual  <- crop(QDS_residuals, GCFR_border_buffered)
+SWAFR_QDS_residual <- crop(QDS_residuals, SWAFR_border_buffered)
+
+Moran(GCFR_QDS_residual)
+my_Moran(GCFR_QDS_residual)
+plot(MoranLocal(GCFR_QDS_residual))
+plot(MoranLocal(GCFR_QDS_residual)[] ~ GCFR_QDS_residual[])
+MoranLocalP(GCFR_QDS_residual)
+plot(GCFR_QDS_residual[][-ncell(GCFR_QDS_residual)] ~ GCFR_QDS_residual[][-1])
+m <- lm(residual ~ x + y + I(x^2) + I(y^2), as.data.frame(cbind(
+  residual = GCFR_QDS_residual[],
+  xyFromCell(GCFR_QDS_residual, 1:ncell(GCFR_QDS_residual))
+)))
+summary(m)
+plot(m)
+visreg::visreg(m, xvar = "x", by = "y", overlay = TRUE)
+visreg::visreg(m, xvar = "y", by = "x", overlay = TRUE)
